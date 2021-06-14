81edb57ccfe3039eb9d81c47595bbf234f10d078
kreiterd <daniel.kreiter@gmx.de>
2021-06-14 10:56:45 +0200

    Add color file

---
+++ lib/colors.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+MaterialColor primaryColor = MaterialColor(0xFF293F69, primaryColorMap);
+
+Map<int, Color> primaryColorMap = {
+  50: Color.fromRGBO(1,63,105, .1),
+  100: Color.fromRGBO(1,63,105, .2),
+  200: Color.fromRGBO(1,63,105, .3),
+  300: Color.fromRGBO(1,63,105, .4),
+  400: Color.fromRGBO(1,63,105, .5),
+  500: Color.fromRGBO(1,63,105, .6),
+  600: Color.fromRGBO(1,63,105, .7),
+  700: Color.fromRGBO(1,63,105, .8),
+  800: Color.fromRGBO(1,63,105, .9),
+  900: Color.fromRGBO(1,63,105, 1),
+};
+
+MaterialColor secondaryColor = MaterialColor(0xFFaeaeb2, secondaryColorMap);
+
+Map<int, Color> secondaryColorMap = {
+  50: Color.fromRGBO(174,174,178, .1),
+  100: Color.fromRGBO(174,174,178, .2),
+  200: Color.fromRGBO(174,174,178, .3),
+  300: Color.fromRGBO(174,174,178, .4),
+  400: Color.fromRGBO(174,174,178, .5),
+  500: Color.fromRGBO(174,174,178, .6),
+  600: Color.fromRGBO(174,174,178, .7),
+  700: Color.fromRGBO(174,174,178, .8),
+  800: Color.fromRGBO(174,174,178, .9),
+  900: Color.fromRGBO(174,174,178, 1),
+};
+
+
+
+
