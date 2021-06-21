48bd0b6b85f4d4d84c5eb362632177b4dab8ba9e
kreiterd <daniel.kreiter@gmx.de>
2021-06-21 11:04:47 +0200

    Add FlowBuilder to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/profile_wizard_flow/repository/profile_wizard_repository.dart';
+import 'package:peerpal/widgets/custom_peerpal_button.dart';
+
+import 'profile_wizard_flow/pages/profile_wizard_flow.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp(profileWizardRepository: ProfileWizardRepository(),));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  MyApp({Key? key, required ProfileWizardRepository profileWizardRepository})
+      : _profileWizardRepository = profileWizardRepository,
+        super(key: key);
+
+  final ProfileWizardRepository _profileWizardRepository;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PeerPAL',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return RepositoryProvider.value(
+      value: _profileWizardRepository,
+      child: MaterialApp(
+        title: 'PeerPAL',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(title: 'PeerPAL App'),
       ),
-      home: MyHomePage(title: 'PeerPAL App'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, this.title}) : super(key: key);
 
-  final String title;
+  final String? title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -34,10 +47,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(widget.title!),
       ),
       body: Center(
-        child: MaterialButton(
+        child: CustomPeerPALButton(
+          text: "ProfileWizardFlow",
           onPressed: () async {
             await Navigator.of(context).push(ProfileWizardFlow.route());
           },
