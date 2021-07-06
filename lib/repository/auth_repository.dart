36a7765fbb34c349a8decf94a4a3ff853368474e
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-07-06 17:03:22 +0200

    Change code according to linter

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -3,32 +3,33 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:peerpal/repository/models/app_user.dart';
 
 class SignUpFailure implements Exception {
+  SignUpFailure({this.message = 'Fehler bei der Registierung'});
+
   final String message;
-  SignUpFailure({this.message = "Fehler bei der Registierung"});
 }
 
 class LoginFailure implements Exception {
+  LoginFailure({this.message = 'Fehler beim Login.'});
+
   final String message;
-  LoginFailure({this.message = "Fehler beim Login."});
 }
 
 class LogoutFailure implements Exception {
+  LogoutFailure({this.message = 'Fehler beim Ausloggen.'});
+
   final String message;
-  LogoutFailure({this.message = "Fehler beim Ausloggen."});
 }
 
 class AuthenticationRepository {
-
-  final firebase_auth.FirebaseAuth _firebaseAuth;
-  FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
   AuthenticationRepository({
     firebase_auth.FirebaseAuth? firebaseAuth,
   }) : _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance;
+
+  final firebase_auth.FirebaseAuth _firebaseAuth;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Stream<AppUser> get user {
     return _firebaseAuth.authStateChanges().map((firebaseUser) {
@@ -37,7 +38,9 @@ class AuthenticationRepository {
   }
 
   AppUser get currentUser {
-    return _firebaseAuth.currentUser == null ? AppUser.empty : _getUserFromFirebaseUser();
+    return _firebaseAuth.currentUser == null
+        ? AppUser.empty
+        : _getUserFromFirebaseUser()!;
   }
 
   Future<void> signUp({required String email, required String password}) async {
@@ -46,19 +49,22 @@ class AuthenticationRepository {
         email: email,
         password: password,
       );
-    }  on FirebaseAuthException catch  (e) {
-      //print('Failed with error code: ${e.code}');
-      //print(e.message);
+    } on FirebaseAuthException catch (e) {
       // https://pub.dev/documentation/firebase_auth/latest/firebase_auth/FirebaseAuth/createUserWithEmailAndPassword.html
-      switch(e.code) {
-        case "email-already-in-use":
-          throw SignUpFailure(message: "Diese E-Mail ist bereits in Benutzung.");
-        case "operation-not-allowed":
-          throw SignUpFailure(message: "Es ist ein Fehler aufgetreten. Registierungen sind deaktiviert.");
-        case "weak-password:":
-          throw SignUpFailure(message: "Das gewählte Passwort ist zu schwach.");
-        case "too-many-requests":
-          throw SignUpFailure(message: "Der Server ist ausgelastet. Bitte versuche es später oder morgen noch einmal.");
+      switch (e.code) {
+        case 'email-already-in-use':
+          throw SignUpFailure(
+              message: "Diese E-Mail ist bereits in Benutzung.");
+        case 'operation-not-allowed':
+          throw SignUpFailure(
+              message: 'Es ist ein Fehler aufgetreten. '
+                  'Registierungen sind deaktiviert.');
+        case 'weak-password:':
+          throw SignUpFailure(message: 'Das gewählte Passwort ist zu schwach.');
+        case 'too-many-requests':
+          throw SignUpFailure(
+              message: 'Der Server ist ausgelastet. Bitte versuche es später '
+                  'oder morgen noch einmal.');
         default:
           throw SignUpFailure();
       }
@@ -76,20 +82,23 @@ class AuthenticationRepository {
         email: email,
         password: password,
       );
-    }  on FirebaseAuthException catch  (e) {
-      //print('Failed with error code: ${e.code}');
-      //print(e.message);
+    } on FirebaseAuthException catch (e) {
       // https://pub.dev/documentation/firebase_auth/latest/firebase_auth/FirebaseAuth/signInWithEmailAndPassword.html
-      switch(e.code) {
-        case "wrong-password":
-        case "user-not-found":
-          throw LoginFailure(message: "Falsches Passwort oder die der Nutzer existiert nicht.");
-        case "user-disabled":
-          throw LoginFailure(message: "Der Account wurde deaktiviert.");
-        case "invalid-email":
-          throw LoginFailure(message: "Die E-Mail ist ungültig.");
-        case "too-many-requests":
-          throw LoginFailure(message: "Der Server ist ausgelastet. Bitte versuche es später oder morgen noch einmal.");
+      switch (e.code) {
+        case 'wrong-password':
+        case 'user-not-found':
+          throw LoginFailure(
+              message:
+                  'Falsches Passwort oder die der Nutzer existiert nicht.');
+        case 'user-disabled':
+          throw LoginFailure(message: 'Der Account wurde deaktiviert.');
+        case 'invalid-email':
+          throw LoginFailure(message: 'Die E-Mail ist ungültig.');
+        case 'too-many-requests':
+          throw LoginFailure(
+              message:
+                  'Der Server ist ausgelastet. Bitte versuche es später oder '
+                  "morgen noch einmal.");
         default:
           throw LoginFailure();
       }
@@ -97,7 +106,6 @@ class AuthenticationRepository {
       throw LoginFailure();
     }
   }
-
 
   Future<void> logout() async {
     try {
@@ -121,14 +129,10 @@ class AuthenticationRepository {
     }, SetOptions(merge: true));
   }
 
-  _getUserFromFirebaseUser() {
-    firebase_auth.User? firebaseUser =
-        firebase_auth.FirebaseAuth.instance.currentUser;
+  AppUser _getUserFromFirebaseUser() {
+    var firebaseUser = firebase_auth.FirebaseAuth.instance.currentUser;
     return (firebaseUser == null
         ? null
         : AppUser(id: firebaseUser.uid, email: firebaseUser.email));
   }
 }
-
-
-
