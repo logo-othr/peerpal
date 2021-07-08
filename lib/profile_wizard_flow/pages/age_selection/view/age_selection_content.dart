d83ea686dc6af2032aa3affc1414d57b941c9e6c
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-07-08 20:37:38 +0200

    Add age_selection_content.dart

---
+++ lib/profile_wizard_flow/pages/age_selection/view/age_selection_content.dart
@@ -0,0 +1,87 @@
+import 'package:flow_builder/flow_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/profile_wizard_flow/pages/age_selection/cubit/age_selection_cubit.dart';
+import 'package:peerpal/repository/models/app_user.dart';
+import 'package:peerpal/widgets/age_picker.dart';
+import 'package:peerpal/widgets/custom_peerpal_button.dart';
+import 'package:peerpal/widgets/custom_peerpal_heading.dart';
+
+class AgeSelectionContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: const Text('Alter'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            BlocBuilder<AgeSelectionCubit, AgeSelectionState>(
+                builder: (context, state) {
+              return Center(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      CustomPeerPALHeading1('Willkommen bei PeerPAL'),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      const SizedBox(
+                        height: 100,
+                        width: 100,
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      CustomPeerPALHeading1('Wie alt bist du?'),
+                      AgePicker(
+                        hint: const Text('Alter auswählen'),
+                        items: state.ages.map((el) => el.toString()).toList(),
+                        value: state.selectedAge,
+                        onChanged: (value) =>
+                            context.read<AgeSelectionCubit>().ageSelected(
+                                  (state.ages[value!]),
+                                ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }),
+            const Spacer(),
+            BlocBuilder<AgeSelectionCubit, AgeSelectionState>(
+              builder: (context, state) {
+                if (state is AgeSelectionPosting) {
+                  return const CircularProgressIndicator();
+                } else {
+                  return CustomPeerPALButton(
+                    text: 'Weiter',
+                    onPressed: () async {
+                      await context
+                          .read<AgeSelectionCubit>()
+                          .postAge(state.selectedAge);
+
+                      var selectedAge = state.selectedAge;
+                      context
+                          .flow<AppUser>()
+                          .update((s) => s.copyWith(age: selectedAge));
+                    },
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
