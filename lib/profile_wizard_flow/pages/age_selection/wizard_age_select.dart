342bd53cf5cce2b084cc8b72f6a2e694a857105d
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-07-08 19:16:03 +0200

    Change age selection form

--- lib/profile_wizard_flow/pages/age_selection/wizard_age_select.dart
+++ lib/profile_wizard_flow/pages/age_selection/wizard_age_select.dart
@@ -1,11 +1,10 @@
+import 'package:flow_builder/flow_builder.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:peerpal/profile_wizard_flow/models/profile.dart';
 import 'package:peerpal/profile_wizard_flow/pages/age_selection/age_selection_cubit.dart';
-import 'package:peerpal/profile_wizard_flow/repository/profile_wizard_repository.dart';
-import 'package:flow_builder/flow_builder.dart';
-import 'package:peerpal/profile_wizard_flow/repository/user_repository.dart';
+import 'package:peerpal/repository/auth_repository.dart';
+import 'package:peerpal/repository/models/app_user.dart';
 import 'package:peerpal/widgets/age_picker.dart';
 import 'package:peerpal/widgets/custom_peerpal_button.dart';
 import 'package:peerpal/widgets/custom_peerpal_heading.dart';
@@ -17,12 +16,14 @@ class AgeSelection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) {
-        // ToDo: Update repository and get it with getit
-        return AgeSelectionCubit(FakeUserRepository());
-      },
-      child: AgeSelectionForm(),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: BlocProvider(
+        create: (_) {
+          return AgeSelectionCubit(context.read<AuthenticationRepository>());
+        },
+        child: AgeSelectionForm(),
+      ),
     );
   }
 }
@@ -32,10 +33,9 @@ class AgeSelectionForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: BackButton(
-          onPressed: () => context.flow<Profile>().complete(),
-        ),
-        title: const Text('Country'),
+        leading: Container(),
+        title: const Text('Alter'),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
@@ -43,42 +43,42 @@ class AgeSelectionForm extends StatelessWidget {
           children: [
             BlocBuilder<AgeSelectionCubit, AgeSelectionState>(
                 builder: (context, state) {
-              return Center(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      SizedBox(
+                  return Center(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          const SizedBox(
                         height: 40,
                       ),
-                      CustomPeerPALHeading1("Willkommen bei PeerPAL"),
-                      SizedBox(
+                          CustomPeerPALHeading1("Willkommen bei PeerPAL"),
+                          const SizedBox(
                         height: 40,
                       ),
-                      Container(
-                        height: 100,
-                        width: 100,
-                        //   decoration: peerpalLogo,
-                      ),
-                      SizedBox(
-                        height: 40,
-                      ),
-                      CustomPeerPALHeading1("Wie alt bist du?"),
-                      AgePicker(
-                        hint: const Text('Alter auswählen'),
-                        items: state.ages.map((el) => el.toString()).toList(),
-                        value: state.selectedAge,
-                        onChanged: (value) =>
-                            context.read<AgeSelectionCubit>().ageSelected(
+                          Container(
+                            height: 100,
+                            width: 100,
+                            //   decoration: peerpalLogo,
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          CustomPeerPALHeading1("Wie alt bist du?"),
+                          AgePicker(
+                            hint: const Text('Alter auswählen'),
+                            items: state.ages.map((el) => el.toString()).toList(),
+                            value: state.selectedAge,
+                            onChanged: (value) =>
+                                context.read<AgeSelectionCubit>().ageSelected(
                                   (state.ages[value!]),
                                 ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-              );
-            }),
+                    ),
+                  );
+                }),
             Spacer(),
             BlocBuilder<AgeSelectionCubit, AgeSelectionState>(
               builder: (context, state) {
@@ -91,11 +91,13 @@ class AgeSelectionForm extends StatelessWidget {
                       await context
                           .read<AgeSelectionCubit>()
                           .postAge(state.selectedAge);
+
+                      int selectedAge = state.selectedAge;
                       context
-                          .flow<Profile>()
-                          .update((s) => s.copyWith(age: state.selectedAge));
-                      print(context
-                          .flow<Profile>().state.age);
+                          .flow<AppUser>()
+                          .update((s) => s.copyWith(age: selectedAge));
+                      AppUser appUser = context.flow<AppUser>().state;
+                      print(appUser.age);
                     },
                   );
                 }
