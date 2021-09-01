b0f0b5a99d4074bca64c5c5d61266cc7f6644058
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-09-01 11:58:24 +0200

    Access UserInformation trough authrepo

--- lib/home/cubit/home_cubit.dart
+++ lib/home/cubit/home_cubit.dart
@@ -1,8 +1,18 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:peerpal/repository/app_user_repository.dart';
+import 'package:peerpal/repository/models/user_information.dart';
 
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeInitial());
+  final AppUserRepository _appuserRepository;
+
+  HomeCubit(this._appuserRepository) : super(HomeInitial());
+
+  Future<void> getCurrentUserInformation() async {
+    emit(HomeLoading());
+    final home = await _appuserRepository.getCurrentUserInformation();
+    emit(HomeLoaded(home));
+  }
 }
