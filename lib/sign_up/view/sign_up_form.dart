4ba6f5b7f00f0044dcf828a3e61d2c229cf53360
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-09-01 12:02:32 +0200

    Change projekt structure

--- lib/sign_up/view/sign_up_form.dart
+++ lib/sign_up/view/sign_up_form.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:formz/formz.dart';
-import 'package:peerpal/password_model.dart';
+import 'package:peerpal/sign_up/models/password_model.dart';
 import 'package:peerpal/sign_up/sign_up.dart';
 import 'package:peerpal/widgets/custom_peerpal_button.dart';
 import 'package:peerpal/widgets/custom_peerpal_heading.dart';
