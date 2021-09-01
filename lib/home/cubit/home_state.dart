96a59e41d1ccc4b9d9565993354152fe24a81daa
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-09-01 11:58:51 +0200

    Add HomeState states

--- lib/home/cubit/home_state.dart
+++ lib/home/cubit/home_state.dart
@@ -1,6 +1,46 @@
 part of 'home_cubit.dart';
 
 @immutable
-abstract class HomeState {}
+abstract class HomeState {
+  const HomeState();
+}
 
-class HomeInitial extends HomeState {}
+class HomeInitial extends HomeState {
+  const HomeInitial();
+}
+
+class HomeLoading extends HomeState {
+  const HomeLoading();
+}
+
+class HomeLoaded extends HomeState {
+  final UserInformation userInformation;
+
+  const HomeLoaded(this.userInformation);
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is HomeLoaded && o.userInformation == userInformation;
+  }
+
+  @override
+  int get hashCode => userInformation.hashCode;
+}
+
+class HomeError extends HomeState {
+  final String message;
+
+  const HomeError(this.message);
+
+  @override
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+
+    return o is HomeError && o.message == message;
+  }
+
+  @override
+  int get hashCode => message.hashCode;
+}
