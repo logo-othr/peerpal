1e17bfc4efe04fc950a7cab3ca1944c810d09c37
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-09-01 12:21:39 +0200

    Change profile flow

--- lib/home/routes/routes.dart
+++ lib/home/routes/routes.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:peerpal/home/view/home_page.dart';
 import 'package:peerpal/profile_wizard_flow/pages/age_selection/view/wizard_age_select.dart';
-import 'package:peerpal/repository/models/app_user.dart';
+import 'package:peerpal/profile_wizard_flow/pages/name_selection/view/name_selection.dart';
+import 'package:peerpal/repository/models/user_information.dart';
 
-List<Page> onGenerateHomeViewPages(AppUser appUser, List<Page<dynamic>> pages) {
+List<Page> onGenerateHomeViewPages(
+    UserInformation userInformation, List<Page<dynamic>> pages) {
   return [
     AgeSelection.page(),
-    if (appUser.name != '') MyTabView.page(),
+    if (userInformation.age != null) NameSelectionPage.page(),
+    if (userInformation.name != null) MyTabView.page(),
   ];
 }
