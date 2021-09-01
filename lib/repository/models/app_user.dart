619fef9790eaf6283391a944d65a7d276fa132e0
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-09-01 12:01:23 +0200

    Remove UserInformation from AppUser class

--- lib/repository/models/app_user.dart
+++ lib/repository/models/app_user.dart
@@ -1,20 +1,13 @@
 import 'package:equatable/equatable.dart';
 
 class AppUser extends Equatable {
-  const AppUser(
-      {required this.id,
-      this.email,
-      this.name,
-      this.age,
-      this.phoneNumber,
-      this.imagePath});
+  const AppUser({
+    required this.id,
+    this.email,
+  });
 
   final String id;
   final String? email;
-  final String? name;
-  final int? age;
-  final String? phoneNumber;
-  final String? imagePath;
 
   static const empty = AppUser(id: '');
 
@@ -23,23 +16,15 @@ class AppUser extends Equatable {
   bool get isNotEmpty => this != AppUser.empty;
 
   @override
-  List<Object?> get props => [email, id, name, age, phoneNumber, imagePath];
+  List<Object?> get props => [email, id];
 
   AppUser copyWith({
     String? email,
     String? id,
-    String? name,
-    int? age,
-    String? phoneNumber,
-    String? imagePath,
   }) {
     return AppUser(
       email: email ?? this.email,
       id: id ?? this.id,
-      age: age ?? this.age,
-      name: name ?? this.name,
-      phoneNumber: phoneNumber ?? this.phoneNumber,
-      imagePath: imagePath ?? this.imagePath,
     );
   }
 }
