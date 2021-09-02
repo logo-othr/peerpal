de4a398f45078ce8a3812160b7a94bc9dc577f77
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-09-02 13:31:49 +0200

    Rename directory age_selection to age_input_page

--- lib/profile_wizard_flow/pages/age_input_page/view/wizard_age_select.dart
+++ lib/profile_wizard_flow/pages/age_input_page/view/wizard_age_select.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:peerpal/profile_wizard_flow/pages/age_selection/cubit/age_selection_cubit.dart';
-import 'package:peerpal/profile_wizard_flow/pages/age_selection/view/age_selection_content.dart';
+import 'package:peerpal/profile_wizard_flow/pages/age_input_page/cubit/age_selection_cubit.dart';
+import 'package:peerpal/profile_wizard_flow/pages/age_input_page/view/age_selection_content.dart';
 import 'package:peerpal/repository/app_user_repository.dart';
 
 class AgeSelection extends StatelessWidget {
