0c50cc0352001e866d7e8e46fd7750d94c4fae9d
kreiterd <daniel.kreiter@gmx.de>
2021-09-07 18:39:28 +0200

    Add UserProfilePicturePath to UserProfile

--- lib/repository/models/user_information.dart
+++ lib/repository/models/user_information.dart
@@ -5,6 +5,7 @@ enum UserInformationField {
   userAge,
   userName,
   userPhoneNumber,
+  userProfilePicturePath,
 }
 
 // ToDo: Move field names from UserDatabaseContract to this extension
@@ -17,18 +18,21 @@ extension UserInformationFieldExtension on UserInformationField {
         return UserDatabaseContract.userName;
       case UserInformationField.userPhoneNumber:
         return UserDatabaseContract.userPhoneNumber;
+      case UserInformationField.userProfilePicturePath:
+        return UserDatabaseContract.userProfilePicturePath;
     }
   }
 }
 
 class UserInformation extends Equatable {
   const UserInformation(
-      {this.name, this.age, this.phoneNumber, this.imagePath});
+      {this.name, this.age, this.phoneNumber, this.imagePath, this.filename});
 
   final String? name;
   final int? age;
   final String? phoneNumber;
   final String? imagePath;
+  final String? filename;
 
   static const empty = UserInformation();
 
@@ -36,20 +40,23 @@ class UserInformation extends Equatable {
 
   bool get isNotEmpty => this != UserInformation.empty;
 
+
   @override
-  List<Object?> get props => [name, age, phoneNumber, imagePath];
+  List<Object?> get props => [name, age, phoneNumber, imagePath, filename];
 
   UserInformation copyWith({
     String? name,
     int? age,
     String? phoneNumber,
     String? imagePath,
+    String?  filename,
   }) {
     return UserInformation(
       age: age ?? this.age,
       name: name ?? this.name,
       phoneNumber: phoneNumber ?? this.phoneNumber,
       imagePath: imagePath ?? this.imagePath,
+        filename: filename ?? this.filename
     );
   }
 }
