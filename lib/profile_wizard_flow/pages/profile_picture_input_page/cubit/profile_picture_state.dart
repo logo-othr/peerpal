9f616cd0407e6070cbf666ca314927002fed5241
kreiterd <daniel.kreiter@gmx.de>
2021-09-07 18:42:31 +0200

    implement profile_picture_state class

--- lib/profile_wizard_flow/pages/profile_picture_input_page/cubit/profile_picture_state.dart
+++ lib/profile_wizard_flow/pages/profile_picture_input_page/cubit/profile_picture_state.dart
@@ -1,10 +1,66 @@
 part of 'profile_picture_cubit.dart';
 
 abstract class ProfilePictureState extends Equatable {
+
   const ProfilePictureState();
+  @override
+  List<Object?> get props => [];
 }
 
 class ProfilePictureInitial extends ProfilePictureState {
+
+  const ProfilePictureInitial() ;
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
+}
+
+class ProfilePictureEmpty extends ProfilePictureState {
+  const ProfilePictureEmpty();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class ProfilePicturePicking extends ProfilePictureState {
+  const ProfilePicturePicking();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class ProfilePicturePicked extends ProfilePictureState {
+  final PickedFile profilePicture;
+
+  const ProfilePicturePicked(this.profilePicture);
+
+  @override
+  List<Object?> get props => [profilePicture];
+}
+
+class ProfilePicturePosting extends ProfilePictureState {
+  final PickedFile profilePicture;
+
+  ProfilePicturePosting(this.profilePicture);
+
+  @override
+  List<Object?> get props => [profilePicture];
+}
+
+class ProfilePicturePosted extends ProfilePictureState {
+  final String profilePictureURL;
+
+  ProfilePicturePosted(this.profilePictureURL);
+
+  @override
+  List<Object?> get props => [profilePictureURL];
+}
+
+class ProfilePictureError extends ProfilePictureState {
+  final String message;
+
+  ProfilePictureError(this.message);
+
+  @override
+  List<Object?> get props => [message];
 }
