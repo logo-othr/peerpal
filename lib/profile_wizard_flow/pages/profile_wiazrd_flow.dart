8fcea38b9e99b6117aa683dda40fea7962d32520
kreiterd <daniel.kreiter@gmx.de>
2021-09-15 20:11:56 +0200

    add: profile_wiazrd_flow.dart

---
+++ lib/profile_wizard_flow/pages/profile_wiazrd_flow.dart
@@ -0,0 +1,25 @@
+import 'package:flow_builder/flow_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:peerpal/home/routes/routes.dart';
+import 'package:peerpal/repository/models/user_information.dart';
+
+class ProfileWizardFlow extends StatelessWidget {
+  final UserInformation flowState;
+
+  const ProfileWizardFlow(this.flowState);
+
+  static Route<UserInformation> route(UserInformation flowSate) {
+    return MaterialPageRoute(builder: (_) => ProfileWizardFlow(flowSate));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: FlowBuilder<UserInformation>(
+        state: flowState,
+        onGeneratePages: onGenerateHomeViewPages,
+      ),
+    );
+  }
+}
