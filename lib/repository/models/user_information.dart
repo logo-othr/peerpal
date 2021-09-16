9284007df90294f5cdaf4e7d6b05b719fc9f73a0
kreiterd <daniel.kreiter@gmx.de>
2021-09-16 14:55:22 +0200

    update: add discoverFromAge, discoverToAge, discoverCommunicationPreferences, discoverActivities and discoverLocations to UserInformation

--- lib/repository/models/user_information.dart
+++ lib/repository/models/user_information.dart
@@ -1,5 +1,20 @@
 import 'package:equatable/equatable.dart';
 import 'package:peerpal/repository/contracts/user_database_contract.dart';
+import 'package:peerpal/repository/models/activity.dart';
+import 'package:peerpal/repository/models/location.dart';
+
+enum CommunicationType { phone, chat }
+
+extension CommunicationTypeExtension on CommunicationType {
+  String get fieldName {
+    switch (this) {
+      case CommunicationType.phone:
+        return 'phone';
+      case CommunicationType.chat:
+        return 'chat';
+    }
+  }
+}
 
 enum UserInformationField {
   age,
@@ -26,12 +41,25 @@ extension UserInformationFieldExtension on UserInformationField {
 
 class UserInformation extends Equatable {
   const UserInformation(
-      {this.name, this.age, this.phoneNumber, this.imagePath});
+      {this.name,
+      this.age,
+      this.phoneNumber,
+      this.imagePath,
+      this.discoverFromAge,
+      this.discoverToAge,
+      this.discoverCommunicationPreferences,
+      this.discoverActivities,
+      this.discoverLocations});
 
   final String? name;
   final int? age;
   final String? phoneNumber;
   final String? imagePath;
+  final int? discoverFromAge;
+  final int? discoverToAge;
+  final Map<CommunicationType, bool>? discoverCommunicationPreferences;
+  final List<Activity>? discoverActivities;
+  final List<Location>? discoverLocations;
 
   static const empty = UserInformation();
 
@@ -39,7 +67,7 @@ class UserInformation extends Equatable {
 
   bool get isNotEmpty => this != UserInformation.empty;
 
-  bool get isComplete {
+  bool get isProfileComplete {
     if (age != null &&
         name != null &&
         phoneNumber != null &&
@@ -50,29 +78,43 @@ class UserInformation extends Equatable {
     }
   }
 
-  bool get isNotComplete => isComplete != true;
+  bool get isNotComplete => isProfileComplete != true;
 
   @override
-  List<Object?> get props => [name, age, phoneNumber, imagePath];
+  List<Object?> get props => [
+        name,
+        age,
+        phoneNumber,
+        imagePath,
+        discoverFromAge,
+        discoverToAge,
+        discoverCommunicationPreferences,
+        discoverActivities,
+        discoverLocations
+      ];
 
   UserInformation copyWith({
     String? name,
     int? age,
     String? phoneNumber,
     String? imagePath,
-    String? filename,
+    int? discoverFromAge,
+    int? discoverToAge,
+    Map<CommunicationType, bool>? discoverCommunicationPreferences,
+    List<Activity>? discoverActivities,
+    List<Location>? discoverLocations,
   }) {
     return UserInformation(
       age: age ?? this.age,
       name: name ?? this.name,
       phoneNumber: phoneNumber ?? this.phoneNumber,
       imagePath: imagePath ?? this.imagePath,
+      discoverFromAge: discoverFromAge ?? this.discoverFromAge,
+      discoverToAge: discoverToAge ?? this.discoverToAge,
+      discoverCommunicationPreferences: discoverCommunicationPreferences ??
+          this.discoverCommunicationPreferences,
+      discoverActivities: discoverActivities ?? this.discoverActivities,
+      discoverLocations: discoverLocations ?? this.discoverLocations,
     );
   }
-
-  UserInformation.fromJson(Map<String, dynamic> json)
-      : name = json[UserInformationField.name],
-        age = json[UserInformationField.age],
-        phoneNumber = json[UserInformationField.phone],
-        imagePath = json[UserInformationField.pictureUrl];
 }
