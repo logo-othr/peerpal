dfca8611c9f78b9325772a7b67b2bc5afa39c24d
kreiterd <daniel.kreiter@gmx.de>
2021-09-21 15:16:35 +0200

    add: CustomCircleListItem for the activity selection

---
+++ lib/widgets/custom_circle_list_icon.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+import 'package:peerpal/colors.dart';
+import 'package:peerpal/widgets/custom_peerpal_heading.dart';
+
+class CustomCircleListItem extends StatelessWidget {
+  String label;
+  IconData? icon;
+  bool active;
+
+  CustomCircleListItem({required this.label, this.icon, required this.active});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: active ? Colors.green: primaryColor,
+                width: 2.0,
+              ),
+            ),
+            child: CircleAvatar(
+              radius: 40,
+              backgroundColor: Colors.white,
+              child: Icon(
+                icon,
+                size: 50,
+                color: Colors.black,
+              ),
+            )),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+          child:
+          CustomPeerPALHeading4(label, color: active ? Colors.green: primaryColor),
+        )
+      ],
+    );
+  }
+}
