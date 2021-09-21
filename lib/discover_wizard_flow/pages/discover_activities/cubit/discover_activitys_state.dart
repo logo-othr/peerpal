34c6e7cf16a05ef6b9b1dc84dceeddfb4db50456
kreiterd <daniel.kreiter@gmx.de>
2021-09-21 15:18:25 +0200

    change: implement discover_activities_state.dart

--- lib/discover_wizard_flow/pages/discover_activities/cubit/discover_activitys_state.dart
+++ lib/discover_wizard_flow/pages/discover_activities/cubit/discover_activitys_state.dart
@@ -1,6 +1,60 @@
-part of 'discover_activitys_cubit.dart';
+part of 'discover_activities_cubit.dart';
 
-@immutable
-abstract class DiscoverActivitysState {}
+abstract class DiscoverActivitiesState extends Equatable {
+  List<Activity> activities;
+  List<Activity> selectedActivities;
+  String searchQuery;
 
-class DiscoverActivitysInitial extends DiscoverActivitysState {}
+  DiscoverActivitiesState(this.activities, this.selectedActivities, this.searchQuery);
+}
+
+class DiscoverActivitiesInitial extends DiscoverActivitiesState {
+  DiscoverActivitiesInitial()
+      : super([], [], '');
+
+  @override
+  List<Object?> get props => [activities, selectedActivities];
+}
+
+class DiscoverActivitiesLoaded extends DiscoverActivitiesState {
+  DiscoverActivitiesLoaded(activities)
+      : super(activities, [], '');
+
+  @override
+  List<Object?> get props => [activities, selectedActivities, searchQuery];
+}
+
+class DiscoverActivitiesSelected extends DiscoverActivitiesState {
+  DiscoverActivitiesSelected(activities, selectedActivities, searchQuery)
+      : super(activities, selectedActivities,searchQuery);
+
+  @override
+  List<Object?> get props => [activities, selectedActivities, searchQuery];
+}
+
+class DiscoverActivitiesPosting extends DiscoverActivitiesState {
+  DiscoverActivitiesPosting(activities, selectedActivities)
+      : super(activities, selectedActivities, '');
+
+  @override
+  List<Object?> get props => [activities, selectedActivities, searchQuery];
+}
+
+class DiscoverActivitiesPosted extends DiscoverActivitiesState {
+  DiscoverActivitiesPosted(activities, selectedActivities)
+      : super(activities, selectedActivities, '');
+
+  @override
+  List<Object?> get props => [activities, selectedActivities, searchQuery];
+}
+
+
+class DiscoverActivitiesError extends DiscoverActivitiesState {
+  final String message;
+
+  DiscoverActivitiesError(this.message)
+      : super([], [], '');
+
+  @override
+  List<Object?> get props => [message];
+}
