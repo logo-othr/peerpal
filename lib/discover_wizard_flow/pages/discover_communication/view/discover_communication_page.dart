33f6d3ffdcd334ec921d4ecccb58a48bde0b5d38
kreiterd <daniel.kreiter@gmx.de>
2021-09-23 11:21:37 +0200

    change: implement discover_communication_page.dart

---
+++ lib/discover_wizard_flow/pages/discover_communication/view/discover_communication_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/discover_wizard_flow/pages/discover_communication/cubit/discover_communication_cubit.dart';
+import 'package:peerpal/discover_wizard_flow/pages/discover_communication/view/discover_communication_content.dart';
+import 'package:peerpal/repository/app_user_repository.dart';
+
+class DiscoverCommunicationPage extends StatelessWidget {
+  final bool isInFlowContext;
+
+  const DiscoverCommunicationPage({required this.isInFlowContext});
+
+  static MaterialPage<void> page({required bool isInFlowContext}) {
+    return MaterialPage<void>(
+        child: DiscoverCommunicationPage(isInFlowContext: isInFlowContext));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+        child: BlocProvider.value(
+          value: DiscoverCommunicationCubit(context.read<AppUserRepository>())..loadCommunications(),
+          child: DiscoverCommunicationContent(isInFlowContext: isInFlowContext),
+        ),
+    );
+  }
+}
