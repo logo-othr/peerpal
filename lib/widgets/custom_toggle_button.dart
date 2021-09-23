05a47e54d0d031cd85d0db635411b5ed516098c4
kreiterd <daniel.kreiter@gmx.de>
2021-09-23 11:20:44 +0200

    add: custom_toggle_button.dart

---
+++ lib/widgets/custom_toggle_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:peerpal/colors.dart';
+import 'package:peerpal/widgets/custom_peerpal_text.dart';
+import 'package:provider/provider.dart';
+
+class CustomToggleButton extends StatelessWidget {
+  bool communicationButtonActive = false;
+  String? text;
+  Color? textColor;
+  double? width;
+  double? height;
+  VoidCallback? onPressed;
+  bool active;
+
+  CustomToggleButton(
+      {this.text,
+        required this.textColor,
+        required this.height,
+        required this.width,
+        required this.onPressed,
+        required this.active});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          minimumSize: Size(width!, height!),
+          backgroundColor: active ? Colors.green : secondaryColor,
+          padding: EdgeInsets.all(0),
+        ),
+        child: CustomPeerPALText(
+          text: text,
+          fontSize: 18,
+          color: textColor,
+          fontWeight: FontWeight.normal,
+        ));
+  }
+}
