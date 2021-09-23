fb1013388e5c57a2661037428832cd55add331ae
kreiterd <daniel.kreiter@gmx.de>
2021-09-23 11:21:23 +0200

    change: implement discover_communication_cubit.dart

--- lib/discover_wizard_flow/pages/discover_communication/cubit/discover_communication_cubit.dart
+++ lib/discover_wizard_flow/pages/discover_communication/cubit/discover_communication_cubit.dart
@@ -1,8 +1,53 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:peerpal/repository/app_user_repository.dart';
+import 'package:peerpal/repository/models/user_information.dart';
 
 part 'discover_communication_state.dart';
 
 class DiscoverCommunicationCubit extends Cubit<DiscoverCommunicationState> {
-  DiscoverCommunicationCubit() : super(DiscoverCommunicationInitial());
+  DiscoverCommunicationCubit(this._appUserRepository)
+      : super(DiscoverCommunicationInitial());
+
+  final AppUserRepository _appUserRepository;
+
+  Future<void> loadCommunications() async {
+    var activities = await _appUserRepository.loadCommunicationList();
+    emit(DiscoverCommunicationSelected(activities, [].cast<CommunicationType>()));
+  }
+
+
+  void addCommunication(CommunicationType communication) {
+    if (state is DiscoverCommunicationSelected) {
+      var updatedCommunicationPreferences = List<CommunicationType>.from(state.selectedCommunicationTypes);
+      updatedCommunicationPreferences.add(communication);
+
+      emit(DiscoverCommunicationSelected(state.communicationTypes,
+          updatedCommunicationPreferences));
+    }
+  }
+
+  void removeCommunication(CommunicationType communication) {
+    if (state is DiscoverCommunicationSelected) {
+      var updatedCommunicationPreferences = List<CommunicationType>.from(state.selectedCommunicationTypes);
+      updatedCommunicationPreferences.remove(communication);
+
+      emit(DiscoverCommunicationSelected(state.communicationTypes,
+          updatedCommunicationPreferences));
+    }
+  }
+
+  Future<void> postCommunications() async {
+    if(state is DiscoverCommunicationSelected) {
+      emit(DiscoverCommunicationPosting(state.communicationTypes, state.selectedCommunicationTypes));
+
+      var userInformation = await _appUserRepository.getCurrentUserInformation();
+      var updatedUserInformation =
+      userInformation.copyWith(discoverCommunicationPreferences: state.selectedCommunicationTypes);
+      await _appUserRepository.updateUserInformation(updatedUserInformation);
+
+      emit(DiscoverCommunicationPosted(state.communicationTypes, state.selectedCommunicationTypes));
+    }
+  }
 }
