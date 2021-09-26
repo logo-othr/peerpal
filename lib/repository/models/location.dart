a96521ad4932534017c0d09f67d701d0ada1f48f
kreiterd <daniel.kreiter@gmx.de>
2021-09-26 11:39:34 +0200

    change: add fromJson method

--- lib/repository/models/location.dart
+++ lib/repository/models/location.dart
@@ -13,9 +13,14 @@ class Location extends Equatable {
   final String? street;
   final String? streetNumber;
 
-  @override
-  // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  factory Location.fromJson(Map<String, dynamic> json) {
+    return Location(
+      place: json['place'] as String,
+      zipcode: json['zipcode'] as String,
+    );
+  }
 
+  @override
+  List<Object?> get props => [place];
 
 }
