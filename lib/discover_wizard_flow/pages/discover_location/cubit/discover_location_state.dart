393c83a72d55eb3cfe93c4a64b418f316be0708b
kreiterd <daniel.kreiter@gmx.de>
2021-09-26 11:38:21 +0200

    change: implement discover_location_state.dart

--- lib/discover_wizard_flow/pages/discover_location/cubit/discover_location_state.dart
+++ lib/discover_wizard_flow/pages/discover_location/cubit/discover_location_state.dart
@@ -1,6 +1,60 @@
 part of 'discover_location_cubit.dart';
 
-@immutable
-abstract class DiscoverLocationState {}
+abstract class DiscoverLocationState extends Equatable {
+  List<Location> locations;
+  List<Location> selectedLocations;
+  List<Location> filteredLocations;
 
-class DiscoverLocationInitial extends DiscoverLocationState {}
+  DiscoverLocationState(this.locations, this.selectedLocations, this.filteredLocations);
+}
+
+class DiscoverLocationInitial extends DiscoverLocationState {
+  DiscoverLocationInitial()
+      : super([], [], []);
+
+  @override
+  List<Object?> get props => [locations, selectedLocations, filteredLocations];
+}
+
+class DiscoverLocationLoaded extends DiscoverLocationState {
+  DiscoverLocationLoaded(locations)
+      : super(locations, [], []);
+
+  @override
+  List<Object?> get props => [locations, selectedLocations, filteredLocations];
+}
+
+class DiscoverLocationSelected extends DiscoverLocationState {
+  DiscoverLocationSelected(locations, selectedLocations, filteredLocations)
+      : super(locations, selectedLocations,filteredLocations);
+
+  @override
+  List<Object?> get props => [locations, selectedLocations, filteredLocations];
+}
+
+class DiscoverLocationPosting extends DiscoverLocationState {
+  DiscoverLocationPosting(locations, selectedLocations)
+      : super(locations, selectedLocations, []);
+
+  @override
+  List<Object?> get props => [locations, selectedLocations, filteredLocations];
+}
+
+class DiscoverLocationPosted extends DiscoverLocationState {
+  DiscoverLocationPosted(locations, selectedLocations)
+      : super(locations, selectedLocations, []);
+
+  @override
+  List<Object?> get props => [locations, selectedLocations, filteredLocations];
+}
+
+
+class DiscoverLocationError extends DiscoverLocationState {
+  final String message;
+
+  DiscoverLocationError(this.message)
+      : super([], [], []);
+
+  @override
+  List<Object?> get props => [message];
+}
