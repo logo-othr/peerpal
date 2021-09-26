3b9a840d71a261114553a5b2eb79a3c06fb8404d
kreiterd <daniel.kreiter@gmx.de>
2021-09-26 11:38:04 +0200

    change: implement discover_location_page.dart

---
+++ lib/discover_wizard_flow/pages/discover_location/view/discover_location_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/discover_wizard_flow/pages/discover_age/cubit/discover_age_cubit.dart';
+import 'package:peerpal/discover_wizard_flow/pages/discover_age/view/discover_age_content.dart';
+import 'package:peerpal/discover_wizard_flow/pages/discover_location/cubit/discover_location_cubit.dart';
+import 'package:peerpal/discover_wizard_flow/pages/discover_location/view/discover_location_content.dart';
+import 'package:peerpal/repository/app_user_repository.dart';
+
+class DiscoverLocationPage extends StatelessWidget {
+  final bool isInFlowContext;
+
+  const DiscoverLocationPage({required this.isInFlowContext});
+
+  static MaterialPage<void> page({required bool isInFlowContext}) {
+    return MaterialPage<void>(
+        child: DiscoverLocationPage(isInFlowContext: isInFlowContext));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: BlocProvider.value(
+        value: DiscoverLocationsCubit(context.read<AppUserRepository>())..loadLocations(),
+        child: DiscoverLocationContent(isInFlowContext: isInFlowContext),
+      ),
+    );
+  }
+}
