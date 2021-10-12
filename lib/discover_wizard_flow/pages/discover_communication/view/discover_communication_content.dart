1ba8f154f553780425cc06381f078917e549a4fb
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-10-12 11:05:53 +0200

    refactor: format code

--- lib/discover_wizard_flow/pages/discover_communication/view/discover_communication_content.dart
+++ lib/discover_wizard_flow/pages/discover_communication/view/discover_communication_content.dart
@@ -11,6 +11,8 @@ import 'package:peerpal/widgets/custom_from_to_age_picker.dart';
 import 'package:peerpal/widgets/custom_peerpal_button.dart';
 import 'package:peerpal/widgets/custom_peerpal_heading.dart';
 import 'package:peerpal/widgets/custom_toggle_button.dart';
+import 'package:peerpal/widgets/peerpal_next_button.dart';
+import 'package:peerpal/widgets/peerpal_save_button.dart';
 
 class DiscoverCommunicationContent extends StatelessWidget {
   final bool isInFlowContext;
@@ -18,93 +20,93 @@ class DiscoverCommunicationContent extends StatelessWidget {
   DiscoverCommunicationContent({Key? key, required this.isInFlowContext})
       : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
-    var hasBackButton = (isInFlowContext) ? false : true;
+    var hasBackButton = isInFlowContext;
     return Scaffold(
         appBar: CustomAppBar(
           'Kommunikation',
           hasBackButton: hasBackButton,
         ),
-        body: BlocBuilder<DiscoverCommunicationCubit, DiscoverCommunicationState>(
-            builder: (context, state) {
-              return Center(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
+        body:
+            BlocBuilder<DiscoverCommunicationCubit, DiscoverCommunicationState>(
+                builder: (context, state) {
+          return Center(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    height: 40,
+                  ),
+                  CustomPeerPALHeading1("Bevorzugte Kommunikationsart"),
+                  Spacer(),
+                  Column(
+                    children: [
                       SizedBox(
-                        height: 40,
+                        height: 10,
                       ),
-                      CustomPeerPALHeading1("Bevorzugte Kommunikationsart"),
-                      Spacer(),
-                      Column(
-                        children: [
-                          SizedBox(height: 10,),
-                          for (CommunicationType communicationType in state.communicationTypes)
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                              child: CustomToggleButton(
-                                text: communicationType.toUIString,
-                                textColor: Colors.white,
-                                height: 45,
-                                width: 200,
-                                onPressed: () {
-                                  if (state.selectedCommunicationTypes
-                                      .contains(communicationType)) {
-                                    context
-                                        .read<DiscoverCommunicationCubit>()
-                                        .removeCommunication(communicationType);
-                                  } else {
-                                    context
-                                        .read<DiscoverCommunicationCubit>()
-                                        .addCommunication(communicationType);
-                                  }
-                                },
-                                active: state.selectedCommunicationTypes
-                                    .contains(communicationType),
-                              ),
-                            ),
-                        ],
-                      ),
-                      Spacer(),
-                      BlocBuilder<DiscoverCommunicationCubit, DiscoverCommunicationState>(
-                        builder: (context, state) {
-                          if (state is DiscoverCommunicationPosting) {
-                            return const CircularProgressIndicator();
-                          } else {
-                            if(isInFlowContext) {
-                              return CustomPeerPALButton(
-                                text: 'Weiter',
-                                onPressed: () async {
-                                  await context
-                                      .read<DiscoverCommunicationCubit>()
-                                      .postCommunications();
-                                  context
-                                      .flow<UserInformation>()
-                                      .complete((s) => s.copyWith(discoverCommunicationPreferences: state.selectedCommunicationTypes));
-                                },
-                              );
-                            } else {
-                              return CustomPeerPALButton(
-                                text: 'Speichern',
-                                onPressed: () async {
-                                  await context
-                                      .read<DiscoverCommunicationCubit>()
-                                      .postCommunications();
-                                  Navigator.pop(context);
-                                },
-                              );
-                            }
-                          }
-                        }
-                      ),
+                      for (CommunicationType communicationType
+                          in state.communicationTypes)
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                          child: CustomToggleButton(
+                            text: communicationType.toUIString,
+                            textColor: Colors.white,
+                            height: 45,
+                            width: 200,
+                            onPressed: () {
+                              if (state.selectedCommunicationTypes
+                                  .contains(communicationType)) {
+                                context
+                                    .read<DiscoverCommunicationCubit>()
+                                    .removeCommunication(communicationType);
+                              } else {
+                                context
+                                    .read<DiscoverCommunicationCubit>()
+                                    .addCommunication(communicationType);
+                              }
+                            },
+                            active: state.selectedCommunicationTypes
+                                .contains(communicationType),
+                          ),
+                        ),
                     ],
                   ),
-                ),
-              );
-            }));
+                  Spacer(),
+                  BlocBuilder<DiscoverCommunicationCubit,
+                      DiscoverCommunicationState>(builder: (context, state) {
+                    if (state is DiscoverCommunicationPosting) {
+                      return const CircularProgressIndicator();
+                    } else if (isInFlowContext) {
+                      return PeerPALSaveButton(
+                        onPressed: () async =>
+                            updateCommunication(state, context),
+                      );
+                    } else {
+                      return PeerPALNextButton(
+                        onPressed: () async =>
+                            updateCommunication(state, context),
+                      );
+                    }
+                  }),
+                ],
+              ),
+            ),
+          );
+        }));
+  }
+
+  Future<void> updateCommunication(
+      DiscoverCommunicationState state, BuildContext context) async {
+    if (isInFlowContext) {
+      await context.read<DiscoverCommunicationCubit>().postCommunications();
+      context.flow<UserInformation>().complete((s) => s.copyWith(
+          discoverCommunicationPreferences: state.selectedCommunicationTypes));
+    } else {
+      await context.read<DiscoverCommunicationCubit>().postCommunications();
+      Navigator.pop(context);
+    }
   }
 }
