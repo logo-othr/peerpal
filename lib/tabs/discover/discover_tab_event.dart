a745ab36042b1adea0f2813186d40042a34df53a
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-10-22 14:34:38 +0200

    implement: GetMatchingUsersEvent class

--- lib/tabs/discover/discover_tab_event.dart
+++ lib/tabs/discover/discover_tab_event.dart
@@ -3,3 +3,8 @@ part of 'discover_tab_bloc.dart';
 abstract class DiscoverTabEvent extends Equatable {
   const DiscoverTabEvent();
 }
+
+class GetMatchingUsersEvent extends DiscoverTabEvent {
+  @override
+  List<Object?> get props => [];
+}
