62ec22a7be3c290f438637574337375a441e4e91
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-10-22 14:33:07 +0200

    add: CustomCupertinoSearchBar

---
+++ lib/widgets/custom_cupertino_search_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:peerpal/widgets/custom_peerpal_heading.dart';
+
+class CustomCupertinoSearchBar extends StatelessWidget {
+  String? heading = " ";
+
+  CustomCupertinoSearchBar({required this.heading});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          heading == null
+              ? Container()
+              : Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                  child: CustomPeerPALHeading1(heading!),
+                ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+            child: CupertinoSearchTextField(
+              placeholder: "Suchen",
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
