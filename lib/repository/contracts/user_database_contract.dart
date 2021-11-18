1ff355b3bd00f4cb00252193fb448f751d00e065
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-11-18 13:44:03 +0100

    change: add id and communication preferences

--- lib/repository/contracts/user_database_contract.dart
+++ lib/repository/contracts/user_database_contract.dart
@@ -1,14 +1,15 @@
 class UserDatabaseContract {
   static const publicUsers = 'publicUserData';
   static const privateUsers = 'privateUserData';
+  static const uid = 'id';
   static const userAge = 'age';
   static const userName = 'name';
   static const userPhoneNumber = 'phone_number';
   static const userProfilePicturePath = 'avatar_path';
   static const discoverFromAge = 'discoverFromAge';
   static const discoverToAge = 'discoverToAge';
-  static const discoverCommunicationPreferences =
-      'discoverCommunicationPreferences';
+  static const chatPreference = 'hasChatCommunicationPreference';
+  static const phonePreference = 'hasChatCommunicationPreference';
   static const discoverActivities = 'discoverActivities';
   static const discoverLocations = 'discoverLocations';
 }
