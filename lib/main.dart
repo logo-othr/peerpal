ac3a4e9a6e00fc2a6b8223c6a71922b347e4b112
Daniel Kreiter <daniel.kreiter@gmx.de>
2021-11-24 14:23:14 +0100

    change: add chat repository to the app level repository provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:peerpal/app/bloc_observer.dart';
 import 'package:peerpal/repository/app_user_repository.dart';
+import 'package:peerpal/repository/chat_repository.dart';
 import 'package:peerpal/repository/memory_cache.dart';
 
 import 'app/app.dart';
@@ -12,24 +13,37 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   final authenticationRepository = AppUserRepository(cache: MemoryCache());
+  final chatRepository = ChatRepository();
   await authenticationRepository.user.first;
   //await DebugHelper.createExampleUsers(appUserRepository: authenticationRepository, emailBase:  'pptestmailbase224', password: 'Abc12345678*');
-  runApp(App(authenticationRepository: authenticationRepository));
+  runApp(App(
+      authenticationRepository: authenticationRepository,
+      chatRepository: chatRepository));
 }
 
 class App extends StatelessWidget {
   const App({
     Key? key,
     required AppUserRepository authenticationRepository,
+    required ChatRepository chatRepository,
   })  : _authenticationRepository = authenticationRepository,
+        _chatRepository = chatRepository,
         super(key: key);
 
   final AppUserRepository _authenticationRepository;
+  final ChatRepository _chatRepository;
 
   @override
   Widget build(BuildContext context) {
-    return RepositoryProvider.value(
-      value: _authenticationRepository,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider.value(
+          value: _authenticationRepository,
+        ),
+        RepositoryProvider.value(
+          value: _chatRepository,
+        )
+      ],
       child: BlocProvider(
         create: (_) => AppBloc(
           appUserRepository: _authenticationRepository,
@@ -39,5 +53,3 @@ class App extends StatelessWidget {
     );
   }
 }
-
-
