a8d8f99311c1bb3c9434c0f1e1b0f97ee75474f6
kreiterd <daniel.kreiter@gmx.de>
2021-12-06 11:20:41 +0100

    change: display old data if not in flow context

--- lib/profile_wizard_flow/pages/age_input_page/cubit/age_input_cubit.dart
+++ lib/profile_wizard_flow/pages/age_input_page/cubit/age_input_cubit.dart
@@ -8,7 +8,7 @@ part 'age_input_state.dart';
 class AgeInputCubit extends Cubit<AgeInputState> {
   AgeInputCubit(this._appUserRepository) : super(AgeInputInitial(10));
 
-  final AppUserRepository _appUserRepository;
+  late final AppUserRepository _appUserRepository;
 
   void dataChanged(int selectedAge) {
     emit(AgeInputInitial(selectedAge));
@@ -21,7 +21,12 @@ class AgeInputCubit extends Cubit<AgeInputState> {
     var updatedUserInformation =
         userInformation.copyWith(age: state.selectedAge);
     await _appUserRepository.updateUserInformation(updatedUserInformation);
-
     emit(AgeInputPosted(state.selectedAge));
+  }
+
+  Future<int?>currentAge() async{
+    var userInformation =
+    await _appUserRepository.getCurrentUserInformation();
+    return userInformation.age;
   }
 }
