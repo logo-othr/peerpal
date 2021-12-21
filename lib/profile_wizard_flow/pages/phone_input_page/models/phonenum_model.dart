332ee979a1522c46190a6d6b801ea8af3801eff9
kreiterd <daniel.kreiter@gmx.de>
2021-12-21 14:25:01 +0100

    add: phone number validation model

---
+++ lib/profile_wizard_flow/pages/phone_input_page/models/phonenum_model.dart
@@ -0,0 +1,22 @@
+import 'package:formz/formz.dart';
+
+enum PhoneError { toShort,toLong }
+
+class PhoneModel extends FormzInput<String, PhoneError> {
+  const PhoneModel.pure() : super.pure('');
+
+  const PhoneModel.dirty([String num = '']) : super.dirty(num);
+
+  @override
+  PhoneError? validator(String? number) {
+    if ( number == null || number.length < 9) {
+      return PhoneError.toShort;
+    }
+    else if(number.length>15){
+      return PhoneError.toLong;
+    }
+    else {
+      return null;
+    }
+  }
+}
