4d0eed54ae234fed1f785e0696ecb9bb16bf2022
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-01-27 18:12:18 +0100

    add: widget custom_activity_header_card.dart

---
+++ lib/widgets/custom_activity_header_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../colors.dart';
+import 'custom_peerpal_heading.dart';
+
+// ignore: must_be_immutable
+class CustomActivityHeaderCard extends StatelessWidget {
+  CustomActivityHeaderCard();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+                top: BorderSide(width: 1, color: secondaryColor),
+                bottom: BorderSide(width: 1, color: secondaryColor))),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: <Widget>[
+                Container(
+                    child: CircleAvatar(
+                      radius: 35,
+                      child: Icon(
+                        Icons.directions_bike,
+                        size: 50,
+                        color: Colors.black,
+                      ),
+                      backgroundColor: Colors.white,
+                    ),
+                    decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: new Border.all(
+                        color: primaryColor,
+                        width: 2.0,
+                      ),
+                    )),
+                SizedBox(
+                  width: 20,
+                ),
+                CustomPeerPALHeading2("Radfahren"),
+              ],
+            ),
+          ),
+        ));
+  }
+}
