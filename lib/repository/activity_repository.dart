39358d83bc81d63a0aac980afa0762fed3f920be
kreiterd <daniel.kreiter@gmx.de>
2022-01-29 18:09:01 +0100

    add: activity icons and codes

--- lib/repository/activity_repository.dart
+++ lib/repository/activity_repository.dart
@@ -1,7 +1,30 @@
-class ActivityRepository {
-  loadActivityList() {}
+import 'package:flutter/material.dart';
+import 'package:peerpal/repository/models/activity.dart';
 
-  updateUserInformation(updatedActivityInformation) {}
+class ActivityRepository {
+  Future<List<Activity>> loadActivityList() async {
+    final List<Activity> activities = [];
+    activities.add(Activity(code: 'shopping', name: "Einkaufen"));
+    activities.add(Activity(code: 'walking', name: "Spazieren"));
+    activities.add(Activity(code: 'music', name: "Musik hören"));
+    activities.add(Activity(code: 'coffee', name: "Kaffeetrinken"));
+    activities.add(Activity(code: 'phone', name: "Telefonieren"));
+    activities.add(Activity(code: 'visit', name: "Besuchen"));
+    activities.add(Activity(code: 'car', name: "Ausflug mit dem Auto"));
+    activities.add(Activity(code: 'tv', name: "Fernsehen schauen"));
+    activities.add(Activity(code: 'garden', name: "Gartenarbeit"));
+    activities.add(Activity(code: 'cooking', name: "Kochen"));
+    activities.add(Activity(code: 'eating', name: "Essen gehen"));
+    activities.add(Activity(code: 'travel', name: "Reisen"));
+    activities.add(Activity(code: 'sightseeing', name: "Sightseeing"));
+    activities.add(Activity(code: 'sport', name: "Sport"));
+    activities.add(Activity(code: 'games', name: "Gesellschaftsspiele"));
+    activities.add(Activity(code: 'culture', name: "Kultur"));
+    activities.add(Activity(code: 'diy', name: "Heimwerken"));
+    return activities;
+  }
+
+  updateActivity(updatedActivityInformation) {}
 
   getActivityInformation() {}
 
