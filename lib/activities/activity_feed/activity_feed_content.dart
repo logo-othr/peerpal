9d0b0ab303cf4344ef25b214265163fb5544d20b
kreiterd <daniel.kreiter@gmx.de>
2022-01-29 18:09:50 +0100

    change: generate activity button functionality

--- lib/activities/activity_feed/activity_feed_content.dart
+++ lib/activities/activity_feed/activity_feed_content.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/activities/activity_feed/bloc/activity_feed_bloc.dart';
 import 'package:peerpal/activities/activity_wizard_flow.dart';
 import 'package:peerpal/colors.dart';
+import 'package:peerpal/repository/app_user_repository.dart';
 import 'package:peerpal/repository/models/activity.dart';
 import 'package:peerpal/widgets/custom_cupertino_search_bar.dart';
 import 'package:peerpal/widgets/custom_empty_list_hint.dart';
 import 'package:peerpal/widgets/custom_peerpal_button.dart';
+import 'package:uuid/uuid.dart';
 
 class ActivityFeedContent extends StatelessWidget {
   const ActivityFeedContent({Key? key}) : super(key: key);
@@ -25,17 +29,35 @@ class ActivityFeedContent extends StatelessWidget {
                         bottom: BorderSide(width: 1, color: secondaryColor))),
                 child: CustomCupertinoSearchBar()),
             Spacer(),
-            CustomEmptyListHint(icon: Icons.nature_people, text: "Es wurden noch keine öffentliche \nAktivität geplant"),
+            CustomEmptyListHint(
+                icon: Icons.nature_people,
+                text: "Es wurden noch keine öffentliche \nAktivität geplant"),
             Spacer(),
             Container(
                 color: Colors.transparent,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
-                    CustomPeerPALButton(
-                      text: "Aktivität planen",
-                      onPressed: () =>             ActivityWizardFlow.route(Activity()),
-                        ,
+                    BlocBuilder<ActivityFeedBloc, ActivityFeedState>(
+                      builder: (context, state) {
+                        return CustomPeerPALButton(
+                            text: "Aktivität planen",
+                            onPressed: () async {
+                              var currentUserName = (await context
+                                      .read<AppUserRepository>()
+                                      .getCurrentUserInformation())
+                                  .name;
+                              await Navigator.of(context)
+                                  .push(ActivityWizardFlow.route(Activity(
+                                id: (Uuid()).v4().toString(),
+                                creatorId: context
+                                    .read<AppUserRepository>()
+                                    .currentUser
+                                    .id,
+                                creatorName: currentUserName,
+                              ))); // ToDo: Move to domain layer
+                            });
+                      },
                     ),
                   ],
                 )),
@@ -45,4 +67,3 @@ class ActivityFeedContent extends StatelessWidget {
     );
   }
 }
-
