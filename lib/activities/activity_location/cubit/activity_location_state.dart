134f1e68bd168e70d0ed3563558231d820650176
kreiterd <daniel.kreiter@gmx.de>
2022-01-30 17:55:51 +0100

    add: activity_location_state.dart

---
+++ lib/activities/activity_location/cubit/activity_location_state.dart
@@ -0,0 +1,45 @@
+part of 'activity_location_cubit.dart';
+
+abstract class ActivityLocationInputState extends Equatable {
+  List<Location> locations;
+  List<Location> selectedLocations;
+  List<Location> filteredLocations;
+
+  ActivityLocationInputState(this.locations, this.selectedLocations, this.filteredLocations);
+
+}
+
+
+
+class ActivityLocationInitial extends ActivityLocationInputState {
+ ActivityLocationInitial()
+     : super([], [], []);
+
+ @override
+ List<Object?> get props => [locations, selectedLocations, filteredLocations];
+}
+
+class ActivityLocationLoaded extends ActivityLocationInputState {
+ ActivityLocationLoaded(List<Location> locations, List<Location>  selectedLocations, List<Location>  filteredLocations)
+     : super(locations, selectedLocations,filteredLocations);
+
+ @override
+ List<Object?> get props => [locations, selectedLocations, filteredLocations];
+}
+
+class ActivityLocationPosting extends ActivityLocationInputState {
+ ActivityLocationPosting(List<Location>  locations, List<Location>  selectedLocations)
+     : super(locations, selectedLocations, []);
+
+ @override
+ List<Object?> get props => [locations, selectedLocations, filteredLocations];
+}
+
+class ActivityLocationPosted extends ActivityLocationInputState {
+ ActivityLocationPosted(List<Location>  locations, List<Location> selectedLocations)
+     : super(locations, selectedLocations, []);
+
+ @override
+ List<Object?> get props => [locations, selectedLocations, filteredLocations];
+}
+
