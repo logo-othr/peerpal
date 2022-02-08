4f12fca1bd3367846dbbe8fccb4365f778a93e41
kreiterd <daniel.kreiter@gmx.de>
2022-02-08 10:58:36 +0100

    add: activity_invitation_state.dart.dart

---
+++ lib/activities/activity_invitation/cubit/activity_invitation_state.dart
@@ -0,0 +1,35 @@
+part of 'activity_invitation_cubit.dart';
+
+@immutable
+abstract class ActivityInvitationState extends Equatable {
+ final Stream<List<PeerPALUser>> friends;
+ final Stream<int> friendRequestsSize;
+ final List<PeerPALUser> invitations;
+
+
+ ActivityInvitationState(this.friends, this.friendRequestsSize, this.invitations);
+}
+
+class ActivityInvitationInitial extends ActivityInvitationState {
+ ActivityInvitationInitial() : super(const Stream.empty(), const Stream.empty(), []);
+
+ @override
+ List<Object?> get props => [friends, friendRequestsSize, invitations];
+}
+
+class ActivityInvitationLoading extends ActivityInvitationState {
+ ActivityInvitationLoading(friends, friendRequestsSize)
+     : super(friends, friendRequestsSize, []);
+
+ @override
+ List<Object?> get props => [friends, friendRequestsSize, invitations];
+}
+
+class ActivityInvitationLoaded extends ActivityInvitationState {
+ ActivityInvitationLoaded(friends, friendRequestsSize, attendees)
+     : super(friends, friendRequestsSize, attendees);
+
+ @override
+ List<Object?> get props => [friends, friendRequestsSize, invitations];
+}
+
