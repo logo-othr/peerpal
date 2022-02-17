4eccfad01dac34b93e01467cd81e8d39f6294848
kreiterd <daniel.kreiter@gmx.de>
2022-02-17 10:47:47 +0100

    add: public_userInformation_dto.g.dart

---
+++ lib/repository/models/public_user_information_dto.g.dart
@@ -0,0 +1,45 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'public_user_information_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PublicUserInformationDTO _$PublicUserInformationDTOFromJson(
+        Map<String, dynamic> json) =>
+    PublicUserInformationDTO(
+      id: json['id'] as String?,
+      name: json['name'] as String?,
+      age: json['age'] as int?,
+      discoverFromAge: json['discoverFromAge'] as int?,
+      discoverToAge: json['discoverToAge'] as int?,
+      hasPhoneCommunicationPreference:
+          json['hasPhoneCommunicationPreference'] as bool? ?? false,
+      hasChatCommunicationPreference:
+          json['hasChatCommunicationPreference'] as bool? ?? false,
+      discoverActivities: (json['discoverActivities'] as List<dynamic>?)
+          ?.map((e) => Activity.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      discoverLocations: (json['discoverLocations'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      imagePath: json['imagePath'] as String?,
+    );
+
+Map<String, dynamic> _$PublicUserInformationDTOToJson(
+        PublicUserInformationDTO instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'age': instance.age,
+      'discoverFromAge': instance.discoverFromAge,
+      'discoverToAge': instance.discoverToAge,
+      'hasPhoneCommunicationPreference':
+          instance.hasPhoneCommunicationPreference,
+      'hasChatCommunicationPreference': instance.hasChatCommunicationPreference,
+      'discoverActivities':
+          instance.discoverActivities?.map((e) => e.toJson()).toList(),
+      'discoverLocations': instance.discoverLocations,
+      'imagePath': instance.imagePath,
+    };
