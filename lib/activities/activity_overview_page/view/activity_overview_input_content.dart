bfb2719e118e7a3a1751ef93253ba9721cf3cecf
kreiterd <daniel.kreiter@gmx.de>
2022-02-18 12:40:22 +0100

    change: pass callbacks to header card public switch

--- lib/activities/activity_overview_page/view/activity_overview_input_content.dart
+++ lib/activities/activity_overview_page/view/activity_overview_input_content.dart
@@ -31,14 +31,25 @@ class OverviewInputContent extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
         child: BlocBuilder<OverviewInputCubit, ActivityOverviewState>(
           builder: (context, state) {
-            if(state is ActivityOverviewLoaded) {
+            if (state is ActivityOverviewLoaded) {
               var activity = state.activity;
               var activityCreator = state.activityCreator;
               var activityAttendees = state.attendees;
+              var cubit = context.read<OverviewInputCubit>();
               return Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  CustomActivityOverviewHeaderCard(ActivityIconData.icons[state.activity.code]!, activity.name!),
+                  CustomActivityOverviewHeaderCard(
+                    icon: ActivityIconData.icons[state.activity.code]!,
+                    heading: activity.name!,
+                    isActive: (state.activity.public ?? false),
+                    onActive: () {
+                      cubit.setActivityToPublic();
+                    },
+                    onInactive: () {
+                      cubit.setActivityToPrivate();
+                    },
+                  ),
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
@@ -49,27 +60,34 @@ class OverviewInputContent extends StatelessWidget {
                               CustomSingleCreatorTable(
                                   heading: "ERSTELLER",
                                   text: activity.creatorName,
-                                  avatar: NetworkImage(activityCreator.imagePath!),
+                                  avatar:
+                                      NetworkImage(activityCreator.imagePath!),
                                   tapIcon: Icons.email),
                               CustomSingleTable(
                                 heading: "DATUM",
-                                text: DateFormat('dd.MM.yyyy').format(activity.date!),
+                                text: DateFormat('dd.MM.yyyy')
+                                    .format(activity.date!),
                                 isArrowIconVisible: true,
                                 onPressed: () {},
                               ),
                               CustomSingleTable(
                                 heading: "UHRZEIT",
-                                text: DateFormat('kk:mm').format(activity.date!),
+                                text:
+                                    DateFormat('kk:mm').format(activity.date!),
                                 isArrowIconVisible: true,
                                 onPressed: () {},
                               ),
                               CustomSingleLocationTable(
                                   heading: "ORT",
                                   text: activity.location!.place,
-                                  subText: "${activity.location!.street} ${activity.location!.streetNumber}"),
+                                  subText:
+                                      "${activity.location!.street} ${activity.location!.streetNumber}"),
                               CustomSingleParticipantsTable(
                                 heading: "TEILNEHMER",
-                                text: activityAttendees.map((e) => e.name).toList().join(", "),
+                                text: activityAttendees
+                                    .map((e) => e.name)
+                                    .toList()
+                                    .join(", "),
                                 isOwnCreatedActivity: true,
                               ),
                               CustomSingleDescriptionTable(
@@ -88,13 +106,16 @@ class OverviewInputContent extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: <Widget>[
                           CustomPeerPALButton(
+                            onPressed: () async =>
+                                context.read<OverviewInputCubit>().postData(),
                             text: "Aktivität erstellen",
                           ),
                         ],
                       ))
                 ],
               );
-            } else return CircularProgressIndicator();
+            } else
+              return CircularProgressIndicator();
           },
         ),
       ),
