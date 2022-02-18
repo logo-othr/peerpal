16d429499663494855fb175f514ac0f3f40f5c73
kreiterd <daniel.kreiter@gmx.de>
2022-02-18 15:40:00 +0100

    refactor

--- lib/tabs/discover/discover_tab_bloc.dart
+++ lib/tabs/discover/discover_tab_bloc.dart
@@ -4,6 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:peerpal/repository/app_user_repository.dart';
 import 'package:peerpal/repository/models/peerpal_user.dart';
+import 'package:rxdart/rxdart.dart';
 
 part 'discover_tab_event.dart';
 
@@ -13,18 +14,31 @@ const limit = 10;
 
 class DiscoverTabBloc extends Bloc<DiscoverTabEvent, DiscoverTabState> {
   final AppUserRepository _appUsersRepository;
+ // StreamController<List<PeerPALUser>> _userStreamController = new BehaviorSubject();
+
 
   DiscoverTabBloc(this._appUsersRepository) : super(DiscoverTabState());
 
   @override
   Stream<DiscoverTabState> mapEventToState(DiscoverTabEvent event) async* {
     if (event is UsersLoaded) {
-      yield await _mapUsersLoadedToState(state);
+      yield await lazyLoadUserList(state);
+
+      // ToDo: Implement infinite list with streams and lazy loading
+     /*   Stream<List<PeerPALUser>> userStream =  _appUsersRepository.getMatchingUsersStream(limit: limit);
+_userStreamController.addStream(userStream);
+        yield state.copyWith(
+            status: DiscoverTabStatus.success,
+            users: _userStreamController.stream,
+            chatRequests: _userFriendRequestStreamController.stream
+        );
+*/
+
     }
   }
 
-  Future<DiscoverTabState> _mapUsersLoadedToState(
-      DiscoverTabState state) async {
+
+  Future<DiscoverTabState> lazyLoadUserList(DiscoverTabState state) async {
     if (state.hasNoMoreUsers) return state;
     try {
       if (state.status == DiscoverTabStatus.initial) {
@@ -42,10 +56,10 @@ class DiscoverTabBloc extends Bloc<DiscoverTabEvent, DiscoverTabState> {
       return users.isEmpty
           ? state.copyWith(hasNoMoreUsers: true)
           : state.copyWith(
-        status: DiscoverTabStatus.success,
-        users: updatedUserList,
-        hasNoMoreUsers: false,
-      );
+              status: DiscoverTabStatus.success,
+              users: updatedUserList,
+              hasNoMoreUsers: false,
+            );
     } on Exception {
       return state.copyWith(status: DiscoverTabStatus.error);
     }
