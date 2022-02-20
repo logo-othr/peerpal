0fdc704dbde5666e1daa86182f9b7cd1647a1466
kreiterd <daniel.kreiter@gmx.de>
2022-02-20 21:16:35 +0100

    bugfix: Fixed an issue where invited users were not displayed in the overview

--- lib/activities/activity_overview_page/cubit/activity_overview_cubit.dart
+++ lib/activities/activity_overview_page/cubit/activity_overview_cubit.dart
@@ -8,20 +8,23 @@ import 'package:peerpal/repository/models/peerpal_user.dart';
 part 'activity_overview_state.dart';
 
 class OverviewInputCubit extends Cubit<ActivityOverviewState> {
-  OverviewInputCubit(this._activityRepository, this._appUserRepository) : super(ActivityOverviewInitial());
+  OverviewInputCubit(this._activityRepository, this._appUserRepository)
+      : super(ActivityOverviewInitial());
 
   final ActivityRepository _activityRepository;
-final AppUserRepository _appUserRepository;
+  final AppUserRepository _appUserRepository;
 
   Future<void> loadData() async {
     Activity activity = _activityRepository.getCurrentActivity();
-    PeerPALUser activityCreator = await _appUserRepository.getUserInformation(activity.creatorId!);
+    PeerPALUser activityCreator =
+        await _appUserRepository.getUserInformation(activity.creatorId!);
     List<PeerPALUser> attendees = [];
 
-    if(activity.attendeeIds != null) {
-      activity.attendeeIds!.forEach((element) async {
-        attendees.add(await _appUserRepository.getUserInformation(element));
-      }) ;
+    if (activity.invitationIds != null) {
+      await Future.forEach<String>(activity.invitationIds!, (element) async {
+        PeerPALUser user = await _appUserRepository.getUserInformation(element);
+        attendees.add(user);
+      });
     }
 
     emit(ActivityOverviewLoaded(activity, activityCreator, attendees));
@@ -37,5 +40,8 @@ final AppUserRepository _appUserRepository;
     _activityRepository.updateActivity(state.activity);
   }
 
+  Future<void> postData() async {
+    await _activityRepository.postActivity(state.activity);
+  }
 
 }
