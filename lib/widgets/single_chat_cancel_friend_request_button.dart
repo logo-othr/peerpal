5c3dd2b88434f0369f2a31b469857a49c6054d62
kreiterd <daniel.kreiter@gmx.de>
2022-02-22 11:01:39 +0100

    add: single_chat_cancel_friend_request_button.dart

---
+++ lib/widgets/single_chat_cancel_friend_request_button.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:peerpal/colors.dart';
+import 'package:peerpal/repository/models/peerpal_user.dart';
+
+import 'package:peerpal/widgets/custom_peerpal_heading.dart';
+
+import 'package:provider/src/provider.dart';
+
+// ignore: must_be_immutable
+class SingleChatCancelFriendRequestButton extends StatelessWidget {
+  SingleChatCancelFriendRequestButton({required this.buttonText, required this.onPressed});
+
+  String buttonText;
+  VoidCallback onPressed;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border:
+          Border(bottom: BorderSide(width: 1, color: secondaryColor))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 25, 0),
+                child: Icon(
+                  Icons.person_add,
+                  color: primaryColor,
+                  size: 25,
+                ),
+              ),
+              CustomPeerPALHeading3(
+                text: buttonText,
+                color: primaryColor,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+            child: TextButton(
+              onPressed: onPressed,
+              style: TextButton.styleFrom(
+                  minimumSize: const Size(50, 20),
+                  backgroundColor: Colors.red,
+                  padding: const EdgeInsets.all(0)),
+              child: const Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Text(
+                  'Abbrechen',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),)
+        ],
+      ),
+    );
+  }
+}
