cdd13a504df4a98a7995e00943d6d4c3c5d3d926
kreiterd <daniel.kreiter@gmx.de>
2022-02-23 22:00:54 +0100

    add: custom_picker.dart

---
+++ lib/widgets/custom_picker.dart
@@ -0,0 +1,87 @@
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+
+class CustomPicker extends CommonPickerModel {
+  String digits(int value, int length) {
+    return '$value'.padLeft(length, "0");
+  }
+
+  CustomPicker({DateTime? currentTime, LocaleType? locale})
+      : super(locale: locale) {
+    this.currentTime = currentTime ?? DateTime.now();
+    this.setLeftIndex(this.currentTime.hour);
+    this.setMiddleIndex(0);
+    this.setRightIndex(this.currentTime.second);
+  }
+
+  @override
+  String? leftStringAtIndex(int index) {
+    if (index >= 0 && index < 24) {
+      return this.digits(index, 2);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  String? middleStringAtIndex(int index) {
+    if (index >= 0 && index < 4) {
+      if (index == 0) {
+        return digits(0, 2);
+      }
+      if (index == 1) {
+        return digits(15, 2);
+      }
+      if (index == 2) {
+        return digits(30, 2);
+      }
+      if (index == 3) {
+        return digits(45, 2);
+      }
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  String? rightStringAtIndex(int index) {
+    if (index >= 0 && index < 60) {
+      return this.digits(index, 2);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  String leftDivider() {
+    return ":";
+  }
+
+  @override
+  String rightDivider() {
+    return "";
+  }
+
+  @override
+  List<int> layoutProportions() {
+    return [100, 100, 1];
+  }
+
+  @override
+  DateTime finalTime() {
+    return currentTime.isUtc
+        ? DateTime.utc(
+        currentTime.year,
+        currentTime.month,
+        currentTime.day,
+        this.currentLeftIndex(),
+        this.currentMiddleIndex(),
+        this.currentRightIndex())
+        : DateTime(
+        currentTime.year,
+        currentTime.month,
+        currentTime.day,
+        this.currentLeftIndex(),
+        this.currentMiddleIndex(),
+        this.currentRightIndex());
+  }
+}
