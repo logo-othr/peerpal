35098d08d304c769db769040a671441ac672eda9
kreiterd <daniel.kreiter@gmx.de>
2022-02-24 19:55:54 +0100

    add: custom_single_description_table_view.dart

---
+++ lib/widgets/custom_single_description_table_view.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:peerpal/widgets/custom_peerpal_heading.dart';
+
+
+import '../../../colors.dart';
+
+
+// ignore: must_be_immutable
+class CustomSingleDescriptionTable extends StatelessWidget {
+  String? heading;
+  String? description;
+  bool? isEditingModus = false;
+  VoidCallback? onPressed;
+
+  CustomSingleDescriptionTable({this.heading, this.description, this.onPressed, this.isEditingModus});
+
+  get nameController => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+        child: Container(
+          color: Colors.transparent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 10, 0, 10),
+                child:
+                CustomPeerPALHeading3(text: heading!, color: secondaryColor),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 5, 15, 0),
+                child: TextField(
+                  readOnly: isEditingModus! ? false : true,
+                  style: TextStyle(fontSize: 15),
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    hintMaxLines: 3,
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: description,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
