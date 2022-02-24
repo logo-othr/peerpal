42ddf7a4a2b51337ef6be8399796700bc99b86d8
kreiterd <daniel.kreiter@gmx.de>
2022-02-24 19:54:53 +0100

    add: custom_empty_list_hint.dart

---
+++ lib/widgets/custom_empty_list_hint.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:peerpal/widgets/custom_peerpal_heading.dart';
+
+import '../../colors.dart';
+
+
+
+// ignore: must_be_immutable
+class CustomEmptyListHint extends StatelessWidget {
+  IconData? icon;
+  String? text;
+
+  CustomEmptyListHint({this.icon, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon, color: secondaryColor, size: 60),
+        SizedBox(height: 20),
+        CustomPeerPALHeading2(text!, color: secondaryColor, textAlign: TextAlign.center,),
+        ],
+    );
+  }
+}
