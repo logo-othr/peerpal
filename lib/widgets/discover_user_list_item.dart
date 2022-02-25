215469f386d56fa0117cf7623a7006b54fd500bd
kreiterd <daniel.kreiter@gmx.de>
2022-02-25 14:19:01 +0100

    refactor

--- lib/widgets/discover_user_list_item.dart
+++ lib/widgets/discover_user_list_item.dart
@@ -18,6 +18,9 @@ class DiscoverUserListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var locationString = locations!.join(", ");
+    var activityString = activities!.join(", ");
+
     return GestureDetector(
       onTap: onPressed,
       child: Align(
@@ -29,125 +32,105 @@ class DiscoverUserListItem extends StatelessWidget {
               border: Border(
                   top: BorderSide(width: 1, color: secondaryColor),
                   bottom: BorderSide(width: 1, color: secondaryColor))),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                      child: Container(
-                          child: CircleAvatar(
-                            radius: 30,
-                            child: Image.network(imageLink!),
-                            backgroundColor: Colors.white,
-                          ),
-                          decoration: new BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: new Border.all(
-                              color: primaryColor,
-                              width: 4,
-                            ),
-                          )),
-                    ),
-                    Container(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 200,
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: FittedBox(
-                                  fit: BoxFit.scaleDown,
-                                  child: CustomPeerPALHeading2(header!,
-                                      color: primaryColor)),
-                            ),
-                          ),
-                          SizedBox(height: 3),
-                          Flexible(
-                            child: Row(
-                              children: [
-                                CustomPeerPALHeading3(
-                                  text: "Ort: ",
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                Container(
-                                  width: 150,
-                                  child: RichText(
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                    text: TextSpan(
-                                      children: <TextSpan>[
-                                        for (var location in locations!)
-                                          TextSpan(
-                                            text: "${location}, ",
-                                            style: new TextStyle(
-                                              fontSize: 16,
-                                              fontFamily: 'Roboto',
-                                              color: Colors.black,
-                                            ),
-                                          ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(height: 2),
-                          Flexible(
-                            child: Row(
-                              children: [
-                                CustomPeerPALHeading3(
-                                  text: "Aktivitäten: ",
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                Container(
-                                  width: 150,
-                                  child: RichText(
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                    text: TextSpan(
-                                      children: <TextSpan>[
-                                        for (var activity in activities!)
-                                          TextSpan(
-                                            text: "${activity}, ",
-                                            style: new TextStyle(
-                                              fontSize: 16,
-                                              fontFamily: 'Roboto',
-                                              color: Colors.black,
-                                            ),
-                                          ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
-                child: Icon(
-                  Icons.arrow_forward_ios,
-                  color: secondaryColor,
-                ),
-              )
-            ],
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(0, 13, 0, 13),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _Avatar(imageLink!),
+                _Content(locationString, activityString),
+                _RightArrow(),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _Content(String locationString, String activityString) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _Heading(),
+          SizedBox(height: 3),
+          _WrappedText(title: "Ort: ", content: locationString),
+          SizedBox(height: 2),
+          _WrappedText(title: "Aktivitäten: ", content: activityString)
+        ],
+      ),
+    );
+  }
+
+  Widget _WrappedText({required String title, required String content}) {
+    var textStyle = TextStyle(
+      fontSize: 16,
+      fontFamily: 'Roboto',
+      color: Colors.black,
+    );
+    return Row(
+      children: [
+        CustomPeerPALHeading3(
+          text: title,
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+        RichText(
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          text: TextSpan(
+            children: <TextSpan>[
+              TextSpan(
+                text: content,
+                style: textStyle,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _Heading() {
+    return Container(
+      // width: 200,
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: CustomPeerPALHeading2(header!, color: primaryColor)),
+      ),
+    );
+  }
+
+  Widget _RightArrow() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+      child: Icon(
+        Icons.arrow_forward_ios,
+        color: secondaryColor,
+      ),
+    );
+  }
+
+  Widget _Avatar(String imageURL) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+      child: Container(
+          child: CircleAvatar(
+            radius: 30,
+            child: Image.network(imageURL),
+            backgroundColor: Colors.white,
+          ),
+          decoration: new BoxDecoration(
+            shape: BoxShape.circle,
+            border: new Border.all(
+              color: primaryColor,
+              width: 4,
+            ),
+          )),
     );
   }
 }
