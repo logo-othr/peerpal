6735196b1181f5acf062bd80482a0bf12123cb79
kreiterd <daniel.kreiter@gmx.de>
2022-02-25 12:08:21 +0100

    add: custom_table_header.dart

---
+++ lib/widgets/custom_table_header.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:peerpal/colors.dart';
+import 'package:peerpal/widgets/custom_peerpal_heading.dart';
+
+
+// ignore: must_be_immutable
+class CustomTableHeader extends StatelessWidget {
+  String? heading;
+
+  CustomTableHeader({this.heading});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+        child: Container(
+          color: Colors.transparent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 10, 0, 10),
+                child: CustomPeerPALHeading3(text: heading!, color: secondaryColor),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    border: Border(
+                        bottom: BorderSide(width: 1, color: secondaryColor))),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
