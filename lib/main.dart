d52a2f2650f9137c18ec7042ef36e43f80c7f38a
kreiterd <daniel.kreiter@gmx.de>
2022-02-27 17:03:21 +0100

    change: add firebase cloud message

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:peerpal/app/bloc_observer.dart';
 import 'package:peerpal/chat/domain/repository/chat_repository.dart';
 import 'package:peerpal/injection.dart';
@@ -10,17 +14,74 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app/app.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+
+void configLocalNotification() {
+  final AndroidInitializationSettings initializationSettingsAndroid =
+  const AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  final IOSInitializationSettings initializationSettingsIOS =
+  const IOSInitializationSettings();
+
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+
+  flutterLocalNotificationsPlugin.initialize(initializationSettings);
+}
+
+
+void showNotification(RemoteNotification remoteNotification) async {
+  const NotificationDetails platformChannelSpecifics =
+  const NotificationDetails(
+      android: AndroidNotificationDetails(
+        'app_notification',
+        'app_notification_channel',
+        'Channel for all app notifications',
+        playSound: true,
+        priority: Priority.high,
+        importance: Importance.max,
+      ),
+      iOS: IOSNotificationDetails());
+
+  print(remoteNotification);
+
+  await flutterLocalNotificationsPlugin.show(
+    0,
+    remoteNotification.title,
+    remoteNotification.body,
+    platformChannelSpecifics,
+    payload: 'Notification Payload',
+  );
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message ${message.data}");
+  print('onResume: $message');
+  if (message.notification != null) {
+    showNotification(message.notification!);
+  }
+  return;
+}
+
 Future<void> main() async {
   Bloc.observer = AppBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  configLocalNotification();
+
   await init();
+
+
+
   final authenticationRepository = sl<AppUserRepository>();
   await authenticationRepository.user.first;
 
   //await DebugHelper.createExampleUsers(appUserRepository: authenticationRepository, emailBase:  'pptestmailbase234', password: 'Abc12345678*');
   runApp(App());
 }
+
 
 class App extends StatelessWidget {
   const App({
