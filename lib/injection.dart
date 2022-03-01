9f282ee0abd47b493739057010c334fc20638195
kreiterd <daniel.kreiter@gmx.de>
2022-03-01 08:15:12 +0100

    change: add activity files to service locator

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,4 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:peerpal/activities/activity_feed/bloc/activity_feed_bloc.dart';
+import 'package:peerpal/activities/activity_joined_list/bloc/activity_joined_list_bloc.dart';
+import 'package:peerpal/activities/activity_request_list/bloc/activity_request_list_bloc.dart';
 import 'package:peerpal/chat/data/repository/chat_repository_firebase.dart';
 import 'package:peerpal/chat/domain/repository/chat_repository.dart';
 import 'package:peerpal/chat/domain/usecases/get_chat_requests_for_user.dart';
@@ -6,6 +9,7 @@ import 'package:peerpal/chat/domain/usecases/get_chats_for_users.dart';
 import 'package:peerpal/chat/domain/usecases/get_userchat_for_chat.dart';
 import 'package:peerpal/chat/presentation/chat_list/bloc/chat_list_bloc.dart';
 import 'package:peerpal/chat/presentation/chat_request_list/bloc/chat_request_list_bloc.dart';
+import 'package:peerpal/repository/activity_repository.dart';
 import 'package:peerpal/repository/app_user_repository.dart';
 import 'package:peerpal/repository/cache.dart';
 import 'package:peerpal/repository/memory_cache.dart';
@@ -29,6 +33,15 @@ Future<void> init() async {
   sl.registerFactory(
     () => ChatRequestListBloc(sl(), sl()),
   );
+  sl.registerFactory(
+        () => ActivityFeedBloc(),
+  );
+  sl.registerFactory(
+        () => ActivityRequestListBloc(),
+  );
+  sl.registerFactory(
+        () => ActivityJoinedListBloc(),
+  );
   // UseCase
   sl.registerLazySingleton(() => GetChatsForUser(sl(), sl()));
   sl.registerLazySingleton(() => GetUserChatForChat(sl(), sl()));
@@ -37,6 +50,9 @@ Future<void> init() async {
   // Repo
   sl.registerLazySingleton<ChatRepository>(
     () => ChatRepositoryFirebase(),
+  );
+  sl.registerLazySingleton<ActivityRepository>(
+        () => ActivityRepository(sharedPreferences),
   );
   sl.registerLazySingleton(() => AppUserRepository(cache: sl()));
 
