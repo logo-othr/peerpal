a93edecc270e89bafe94d38831b92fd58dbacb3d
kreiterd <daniel.kreiter@gmx.de>
2022-03-01 08:30:22 +0100

    implement

--- lib/activities/activity_feed/bloc/activity_feed_state.dart
+++ lib/activities/activity_feed/bloc/activity_feed_state.dart
@@ -2,19 +2,38 @@ part of 'activity_feed_bloc.dart';
 
 enum ActivityFeedStatus { initial, success, error }
 
-abstract class ActivityFeedState extends Equatable {
+class ActivityFeedState extends Equatable {
+  const ActivityFeedState({
+    this.status = ActivityFeedStatus.initial,
+    this.activityStream =  const Stream.empty(),
+    this.activityRequestList =  const Stream.empty(),
+    this.activityJoinedList =  const Stream.empty(),
+});
+
+  final ActivityFeedStatus status;
+  final Stream<List<Activity>> activityStream;
+  final Stream<List<Activity>> activityRequestList;
+  final Stream<List<Activity>> activityJoinedList;
+
+  ActivityFeedState copyWith({
+    ActivityFeedStatus? status,
+    Stream<List<Activity>>? activityStream,
+    Stream<List<Activity>>? activityRequestList,
+    Stream<List<Activity>>? activityJoinedList,
+
+
+  }) {
+    return ActivityFeedState(
+        status: status ?? this.status,
+        activityStream: activityStream ?? this.activityStream,
+        activityRequestList: activityRequestList ?? this.activityRequestList,
+        activityJoinedList: activityJoinedList ?? this.activityJoinedList,
+
+
+    );
+  }
+
   @override
-  List<Object?> get props => [];
+  List<Object> get props => [status, activityStream, activityRequestList, activityJoinedList];
 }
 
-class ActivityFeedInitial extends ActivityFeedState {
-  @override
-  List<Object?> get props => [];
-}
-
-class ActivityFeedLoading extends ActivityFeedState {
-  ActivityFeedLoading();
-
-  @override
-  List<Object?> get props => [];
-}
