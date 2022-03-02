77217e4120c3458f197f5ce77af3922528432396
kreiterd <daniel.kreiter@gmx.de>
2022-03-02 12:17:40 +0100

    add: emoji_button.dart

---
+++ lib/widgets/emoji_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmojiButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final String emojiCode;
+
+  const EmojiButton(
+      {Key? key, this.onPressed, required this.emojiCode})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(onTap: onPressed, child: Container(padding:EdgeInsets.all(5),child: Text(emojiCode, style: TextStyle(fontSize: 22),)));
+  }
+}
