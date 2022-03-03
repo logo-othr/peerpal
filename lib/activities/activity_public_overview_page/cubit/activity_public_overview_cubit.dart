dc4bf22fa94f5aaae3cb445683883fc4af5e262f
kreiterd <daniel.kreiter@gmx.de>
2022-03-03 10:48:00 +0100

    add: activity_public_overview_cubit.dart

---
+++ lib/activities/activity_public_overview_page/cubit/activity_public_overview_cubit.dart
@@ -0,0 +1,73 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:peerpal/repository/activity_repository.dart';
+import 'package:peerpal/repository/app_user_repository.dart';
+import 'package:peerpal/repository/models/activity.dart';
+import 'package:peerpal/repository/models/peerpal_user.dart';
+
+part 'activity_public_overview_state.dart';
+
+class ActivityPublicOverviewCubit extends Cubit<ActivityPublicOverviewState> {
+  ActivityPublicOverviewCubit(this._activityRepository, this._appUserRepository)
+      : super(ActivityPublicOverviewInitial());
+
+  final ActivityRepository _activityRepository;
+  final AppUserRepository _appUserRepository;
+
+  Future<void> loadData(Activity activity) async {
+    bool isAttendee = false;
+    if (activity.attendeeIds != null) {
+      if (activity.attendeeIds!.contains(_appUserRepository.currentUser.id)) {
+        isAttendee = true;
+      }
+    }
+
+    PeerPALUser activityCreator =
+        await _appUserRepository.getUserInformation(activity.creatorId!);
+
+    List<PeerPALUser> attendees = [];
+
+    if (activity.attendeeIds != null) {
+      await Future.forEach<String>(activity.attendeeIds!, (element) async {
+        PeerPALUser user = await _appUserRepository.getUserInformation(element);
+        attendees.add(user);
+      });
+    }
+
+    emit(ActivityPublicOverviewLoaded(
+        activity, activityCreator, attendees, isAttendee));
+  }
+
+  Future<void> joinActivity() async {
+    List<String> attendees;
+    Activity? activity;
+    if (state.activity.attendeeIds != null) {
+      attendees = [];
+      attendees.addAll(state.activity.attendeeIds!);
+      attendees.add(_appUserRepository.currentUser.id);
+      activity = state.activity.copyWith(attendeeIds: attendees);
+    }
+    Activity updatedActivity = (activity ?? state.activity);
+
+    await _activityRepository.joinActivity(state.activity);
+
+    emit(ActivityPublicOverviewLoaded(
+        updatedActivity, state.activityCreator, state.attendees, true));
+  }
+
+  Future<void> leaveActivity() async {
+    List<String> attendees;
+    Activity? activity;
+    if (state.activity.attendeeIds != null) {
+      attendees = [];
+      attendees.addAll(state.activity.attendeeIds!);
+      attendees.remove(_appUserRepository.currentUser.id);
+      activity = state.activity.copyWith(attendeeIds: attendees);
+    }
+    Activity updatedActivity = (activity ?? state.activity);
+
+    await _activityRepository.leaveActivity(state.activity);
+    emit(ActivityPublicOverviewLoaded(
+        updatedActivity, state.activityCreator, state.attendees, false));
+  }
+}
