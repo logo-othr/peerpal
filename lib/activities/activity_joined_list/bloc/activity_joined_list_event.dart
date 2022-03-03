29f4c948f6f2962d2da7230995577e07ecb1bb74
kreiterd <daniel.kreiter@gmx.de>
2022-03-03 10:47:09 +0100

    add: activity_joined_list_event.dart

---
+++ lib/activities/activity_joined_list/bloc/activity_joined_list_event.dart
@@ -0,0 +1,13 @@
+part of 'activity_joined_list_bloc.dart';
+
+
+@immutable
+abstract class ActivityJoinedListEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class LoadActivityJoinedList extends ActivityJoinedListEvent {}
+
+
+
