b6188ec7a93490664080973327868cdab61afd5b
kreiterd <daniel.kreiter@gmx.de>
2022-03-03 10:48:48 +0100

    add: activity_request_list_event.dart

---
+++ lib/activities/activity_request_list/bloc/activity_request_list_event.dart
@@ -0,0 +1,13 @@
+part of 'activity_request_list_bloc.dart';
+
+
+@immutable
+abstract class ActivityRequestListEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class LoadActivityRequestList extends ActivityRequestListEvent {}
+
+
+
