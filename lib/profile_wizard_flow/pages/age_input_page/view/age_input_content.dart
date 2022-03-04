aca5a779841a72fa6c1fa0585b0f9d7a5da43348
kreiterd <daniel.kreiter@gmx.de>
2022-03-04 15:06:55 +0100

    add: strings

--- lib/profile_wizard_flow/pages/age_input_page/view/age_input_content.dart
+++ lib/profile_wizard_flow/pages/age_input_page/view/age_input_content.dart
@@ -22,64 +22,84 @@ class AgeInputContent extends StatelessWidget {
           hasBackButton: hasBackButton,
         ),
         body: BlocBuilder<AgeInputCubit, AgeInputState>(
-          buildWhen: (previous, current) => previous.selectedAge != current.selectedAge,
+            buildWhen: (previous, current) =>
+                previous.selectedAge != current.selectedAge,
             builder: (context, state) {
               return new FutureBuilder(
-              future: context.read<AgeInputCubit>().currentAge(),
-              initialData: 14,
-              builder:(BuildContext context, AsyncSnapshot<int?>text) {
-                return Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Center(
-                        child: Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: <Widget>[
-                              const SizedBox(
-                                height: 40,
+                  future: context.read<AgeInputCubit>().currentAge(),
+                  initialData: 14,
+                  builder: (BuildContext context, AsyncSnapshot<int?> text) {
+                    return Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  const SizedBox(
+                                    height: 40,
+                                  ),
+                                  CustomPeerPALHeading1(
+                                      'Willkommen bei PeerPAL'),
+                                  const SizedBox(
+                                    height: 40,
+                                  ),
+                                  Align(
+                                      alignment: Alignment.center,
+                                      child: Container(
+                                          padding: EdgeInsets.all(10),
+                                          height: 120,
+                                          width: 120,
+                                          child: Image(
+                                              image: AssetImage(
+                                                  'assets/PeerPAL-Logo.png')))),
+                                  const SizedBox(
+                                    height: 10,
+                                    width: 100,
+                                  ),
+                                  const SizedBox(
+                                    height: 40,
+                                  ),
+                                  CustomPeerPALHeading1('Wie alt bist du?'),
+                                  new AgePicker(
+                                      hint: const Text('Alter auswählen'),
+                                      items: state.ages
+                                          .map((el) => el.toString())
+                                          .toList(),
+                                      value: state.selectedAge,
+                                      start: isInFlowContext
+                                          ? state.selectedAge
+                                          : text.data,
+                                      onChanged: (value) {
+                                        return context
+                                            .read<AgeInputCubit>()
+                                            .dataChanged(
+                                              (state.ages[value!]),
+                                            );
+                                      }),
+                                ],
                               ),
-                              CustomPeerPALHeading1('Willkommen bei PeerPAL'),
-                              const SizedBox(
-                                height: 40,
-                              ),
-                              const SizedBox(
-                                height: 100,
-                                width: 100,
-                              ),
-                              const SizedBox(
-                                height: 40,
-                              ),
-                              CustomPeerPALHeading1('Wie alt bist du?'),
-                              new AgePicker(
-                                hint: const Text('Alter auswählen'),
-                                items: state.ages.map((el) => el.toString())
-                                    .toList(),
-                                value: state.selectedAge,
-                                start: isInFlowContext? state.selectedAge : text.data,
-                                onChanged: (value){
-                                    return context.read<AgeInputCubit>().dataChanged(
-                                      (state.ages[value!]),
-                                    );}
-                              ),
-                            ],
+                            ),
                           ),
-                        ),
+                          const Spacer(),
+                          (state is AgeInputPosting)
+                              ? const CircularProgressIndicator()
+                              : Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+                                  child: CompletePageButton(
+                                      isSaveButton: isInFlowContext,
+                                      onPressed: () async {
+                                        _update(state, context);
+                                      }),
+                                ),
+                        ],
                       ),
-                      const Spacer(),
-                      (state is AgeInputPosting)
-                          ? const CircularProgressIndicator()
-                          : CompletePageButton(
-                          isSaveButton: isInFlowContext,
-                          onPressed: () async {
-                            _update(state, context);
-                          }),
-                    ],
-                  ),);
-              });
-        }));
+                    );
+                  });
+            }));
   }
 
   Future<void> _update(AgeInputState state, BuildContext context) async {
