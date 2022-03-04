8457eaab0394e9075aa236f57db2f885b7443307
kreiterd <daniel.kreiter@gmx.de>
2022-03-04 15:01:46 +0100

    add: strings

---
+++ lib/strings.dart
@@ -0,0 +1,6 @@
+class Strings {
+  static final  String searchDisabled = "Suche derzeit noch deaktiviert";
+  static final String applicationLegalese =
+      "Urheberrecht (c) Ostbayerische Technische Hochschule Regensburg und Katholische Hochschule Mainz - Gefördert durch das Bundesministerium für Bildung und Forschung (BMBF) FKZ 13FH515SA7 (KH Mainz) und FKZ 13FH515SB7 (OTH Regensburg). ";
+
+}
