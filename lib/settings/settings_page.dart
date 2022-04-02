e1b72413415784102c7fe5bb3d48558c395afec4
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-04-02 16:09:45 +0200

    ui changes, remove debug information

--- lib/settings/settings_page.dart
+++ lib/settings/settings_page.dart
@@ -46,161 +46,138 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar("Einstellungen", hasBackButton: false),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 10),
-            Align(
-                alignment: Alignment.center,
-                child: Container(
-                    padding: EdgeInsets.all(10),
-                    height: 120,
-                    width: 120,
-                    child:
-                    Image(image: AssetImage('assets/peerpal_logo.png')))),
-            SizedBox(height: 10),
-            Divider(thickness: 1, color: primaryColor),
-            SizedBox(height: 10),
-            CustomTableHeader(heading: "PERSÖNLICHE DATEN"),
-            CustomTableRow(
-              text: "Profil",
-              onPressed: () async => {
-                await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ProfileOverviewPage()),
-                )
-              },
-            ),
-            CustomTableRow(
-              text: "Interessen",
-              onPressed: () async => {
-                await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => DiscoverInterestsOverviewPage()),
-                )/*.then((value) =>
-                    context.read<DiscoverTabBloc>().add(ReloadUsers()))*/
-              },
-            ),
-            CustomTableHeader(heading: "RECHTLICHES"),
-            CustomTableRow(
-              text: "Datenschutzerklärung",
-              onPressed: () async => {
-                await Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PrivacyPolicyPage()),
-                ),
-              },
-            ),
-            CustomTableRow(
-              text: "Impressum",
-              onPressed: () async => {
-                await Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ImprintPage()),
-                ),
-              },
-            ),
+      body: Scrollbar(
+        isAlwaysShown: true,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                      padding: EdgeInsets.all(10),
+                      height: 120,
+                      width: 120,
+                      child:
+                      Image(image: AssetImage('assets/peerpal_logo.png')))),
+              SizedBox(height: 10),
+              Divider(thickness: 1, color: primaryColor),
+              SizedBox(height: 10),
+              CustomTableHeader(heading: "PERSÖNLICHE DATEN"),
+              CustomTableRow(
+                text: "Profil",
+                onPressed: () async => {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ProfileOverviewPage()),
+                  )
+                },
+              ),
+              CustomTableRow(
+                text: "Interessen",
+                onPressed: () async => {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DiscoverInterestsOverviewPage()),
+                  )/*.then((value) =>
+                      context.read<DiscoverTabBloc>().add(ReloadUsers()))*/
+                },
+              ),
+              CustomTableHeader(heading: "RECHTLICHES"),
+              CustomTableRow(
+                text: "Datenschutzerklärung",
+                onPressed: () async => {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PrivacyPolicyPage()),
+                  ),
+                },
+              ),
+              CustomTableRow(
+                text: "Impressum",
+                onPressed: () async => {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ImprintPage()),
+                  ),
+                },
+              ),
 /*          FutureBuilder<String>(
-              future: getPlattformData(), // async work
-              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting: return CustomTableRow(
-                    text: "Wird geladen...",
-                  );
-                  default:
-                    if (snapshot.hasError)
-                      return Text('Error: ${snapshot.error}');
-                    else
-                      return           CustomTableRow(
-                        text: "Lizenzen",
-                        onPressed: () async => {
-                          showLicensePage(
-                              context: context,
-                              applicationName: "PeerPAL",
-                              applicationVersion: "0.01 - BETA",
-                              applicationLegalese: applicationLegalese + snapshot.data.toString())
-                        },
-                      );
-                }
-              },
-            ),*/
-            CustomTableRow(
-              text: "Lizenzen",
-              onPressed: () async => {
-                showLicensePage(
-                    context: context,
-                    applicationName: "PeerPAL",
-                    applicationVersion: "Testversion 2 - BETA 1.0.0 (2)",
-                    applicationLegalese: Strings.applicationLegalese)
-              },
-            ),
-            CustomTableRow(
-              text: "Logout",
-              onPressed: () async => {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return CustomDialog(
-                          dialogHeight: 250,
-                          actionButtonText: 'Ausloggen',
-                          dialogText: "Möchten Sie sich wirklich ausloggen?",
-                          onPressed: () => {
-                            context
-                                .read<AppBloc>()
-                                .add(AppLogoutRequested()),
-                          });
-                    }),
-              },
-            ),
-            Column(
-              children: [
-                CustomTableHeader(heading: "SONSTIGES"),
-                CustomTableRow(
-                  text:
-                  "E-Mail: ${sl.get<AppUserRepository>().currentUser.email}",
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                CustomTableHeader(heading: "Entwickler-Informationen"),
-                CustomTableRow(
-                  text: "ID: ${sl.get<AppUserRepository>().currentUser.id}",
-                ),
-                FutureBuilder<PeerPALUser>(
-                  future:
-                  sl.get<AppUserRepository>().getCurrentUserInformation(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<PeerPALUser> snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        return CustomTableRow(
-                          text: "Wird geladen...",
+                future: getPlattformData(), // async work
+                builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting: return CustomTableRow(
+                      text: "Wird geladen...",
+                    );
+                    default:
+                      if (snapshot.hasError)
+                        return Text('Error: ${snapshot.error}');
+                      else
+                        return           CustomTableRow(
+                          text: "Lizenzen",
+                          onPressed: () async => {
+                            showLicensePage(
+                                context: context,
+                                applicationName: "PeerPAL",
+                                applicationVersion: "0.01 - BETA",
+                                applicationLegalese: applicationLegalese + snapshot.data.toString())
+                          },
                         );
-                      default:
-                        if (snapshot.hasError)
-                          return Text('Error: ${snapshot.error}');
-                        else
-                          return Container(
-                            color: Colors.amber,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                SelectableText(
-                                    "Debug-Informationen:\nCache: \n ${sl.get<AppUserRepository>().cache.toString()}  \nAuthUser:\n${sl.get<AppUserRepository>().currentUser.toString()}\nUser:\n${snapshot.data.toString()}"),
-                              ],
-                            ),
-                          );
-                    }
-                  },
-                ),
-              ],
-            ),
-          ],
+                  }
+                },
+              ),*/
+              CustomTableRow(
+                text: "Lizenzen",
+                onPressed: () async => {
+                  showLicensePage(
+                      context: context,
+                      applicationName: "PeerPAL",
+                      applicationVersion: "Testversion 2 - BETA 1.0.0 (2)",
+                      applicationLegalese: Strings.applicationLegalese)
+                },
+              ),
+              CustomTableRow(
+                text: "Logout",
+                onPressed: () async => {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return CustomDialog(
+                            dialogHeight: 250,
+                            actionButtonText: 'Ausloggen',
+                            dialogText: "Möchten Sie sich wirklich ausloggen?",
+                            onPressed: () => {
+                              context
+                                  .read<AppBloc>()
+                                  .add(AppLogoutRequested()),
+                            });
+                      }),
+                },
+              ),
+              Column(
+                children: [
+                  CustomTableHeader(heading: "SONSTIGES"),
+                  CustomTableRow(
+                    text:
+                    "E-Mail: ${sl.get<AppUserRepository>().currentUser.email}",
+                    isArrowVisible: false,
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  CustomTableHeader(heading: "User-ID"),
+                  CustomTableRow(
+                    text: "${sl.get<AppUserRepository>().currentUser.id}",
+                    isArrowVisible: false,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
