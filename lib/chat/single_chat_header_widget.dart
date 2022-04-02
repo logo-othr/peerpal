1b8402dd3db0f770537e191378c8182d0bb1b70e
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-04-02 16:10:08 +0200

    add back arrow icon

--- lib/chat/single_chat_header_widget.dart
+++ lib/chat/single_chat_header_widget.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:peerpal/colors.dart';
 
 class SingleChatHeader extends StatelessWidget {
   final String? name;
@@ -13,43 +14,63 @@ class SingleChatHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Container(
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          border: Border(
-            bottom: BorderSide(
-              color: Colors.grey,
-              width: 1.0,
+    decoration: const BoxDecoration(
+      color: Colors.white,
+      border: Border(
+        bottom: BorderSide(
+          color: Colors.grey,
+          width: 1.0,
+        ),
+      ),
+    ),
+    height: 68,
+    child: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          Container(
+            width: 30,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
             ),
           ),
-        ),
-        height: 68,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Row(
-            children: [
-              ClipOval(
-                child: CircleAvatar(
-                  backgroundColor: Colors.white,
-                    radius: 25, child: CachedNetworkImage(imageUrl: urlAvatar!, errorWidget: (context, object, stackTrace) {
-                  return const Icon(
-                    Icons.account_circle,
-                    size: 40.0,
-                    color: Colors.grey,
-                  );
-                },)),
-              ),
-              const SizedBox(width: 15),
-              Text(
-                name!,
-                style: const TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-                overflow: TextOverflow.ellipsis,
-              ),
-            ],
+
+          ClipOval(
+            child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 25, child: CachedNetworkImage(imageUrl: urlAvatar!, errorWidget: (context, object, stackTrace) {
+              return const Icon(
+                Icons.account_circle,
+                size: 40.0,
+                color: Colors.grey,
+              );
+            },)),
           ),
-        ),
-      );
+          const SizedBox(width: 15),
+          Flexible(
+            child: RichText(
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                    text: name!,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'CustomPeerPalFontFamily',
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
