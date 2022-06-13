a5c16bdd49fc3983a93cfa491a136376a243f0fe
kreiterd <daniel.kreiter@gmx.de>
2022-06-13 11:29:40 +0200

    add barrel files and cleanup

---
+++ lib/login_flow/domain/domain.dart
@@ -0,0 +1,4 @@
+//GENERATED BARREL FILE
+export './models/confirmed_password_model.dart';
+export './models/email_model.dart';
+export './models/password_model.dart';
---
+++ lib/login_flow/presentation/presentation.dart
@@ -0,0 +1,7 @@
+//GENERATED BARREL FILE
+export './login/bloc/login_cubit.dart';
+export './login/view/login_form.dart';
+export './login/view/login_page.dart';
+export './signup/bloc/sign_up_cubit.dart';
+export './signup/view/sign_up_form.dart';
+export './signup/view/sign_up_page.dart';
