1bfee725af7f5f3816e9b59efc18ef0ddfc33904
kreiterd <daniel.kreiter@gmx.de>
2022-06-28 08:08:34 +0200

    move activity_feed and activity_setup to activity folder

---
+++ lib/activity/activity_feed/activity_feed_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/activity/activity_feed/activity_feed_content.dart';
+import 'package:peerpal/activity/activity_feed/bloc/activity_feed_bloc.dart';
+import 'package:peerpal/injection.dart';
+
+class ActivityFeedPage extends StatelessWidget {
+  const ActivityFeedPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: BlocProvider<ActivityFeedBloc>(
+        child: ActivityFeedContent(),
+        create: (context) => sl<ActivityFeedBloc>()..add(LoadActivityFeed()),
+      ),
+    );
+  }
+}
---
+++ lib/activity/activity_setup/activity_location/cubit/activity_location_cubit.dart
@@ -0,0 +1,108 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:peerpal/repository/activity_repository.dart';
+import 'package:peerpal/repository/models/activity.dart';
+import 'package:peerpal/repository/models/location.dart';
+
+part 'activity_location_state.dart';
+
+class ActivityLocationCubit extends Cubit<ActivityLocationInputState> {
+  ActivityLocationCubit(this._activityRepository)
+      : super(ActivityLocationInitial());
+
+  final ActivityRepository _activityRepository;
+
+  Future<void> loadData() async {
+    var locations = await _activityRepository.loadLocations();
+    var activity = await _activityRepository.getCurrentActivity();
+    var locationList = activity.location != null
+        ? <Location>[activity.location!]
+        : <Location>[].cast<Location>();
+    emit(ActivityLocationLoaded(
+        locations, locationList, <Location>[].cast<Location>()));
+  }
+
+  void searchQueryChanged(String searchQuery) {
+    if (state is ActivityLocationLoaded) {
+      var filteredLocations =
+          _searchForLocationsStartingWith(searchQuery, state.locations);
+      for (var location in state.selectedLocations) {
+        filteredLocations.remove(location);
+      }
+      emit(ActivityLocationLoaded(
+          state.locations, state.selectedLocations, filteredLocations));
+    }
+  }
+
+  List<Location> _searchForLocationsStartingWith(
+      String searchQuery, List<Location> locationsFromJSON) {
+    var locations = <Location>[];
+    var allActivityLocations = List<Location>.from((locationsFromJSON));
+
+    if (searchQuery.trim() != "") {
+      searchQuery = searchQuery.toLowerCase();
+
+      locations.addAll(_filterSearchResults(searchQuery, allActivityLocations));
+    }
+    return locations;
+  }
+
+  List<Location> _filterSearchResults(
+      String searchQuery, List<Location> locations) {
+    List<Location> locationData = [];
+    locations.forEach((location) {
+      if (location.place.toString().toLowerCase().startsWith(searchQuery) ||
+          location.zipcode.toString().toLowerCase().startsWith(searchQuery)) {
+        if (!locationData.contains(location)) locationData.add(location);
+      }
+    });
+    return locationData;
+  }
+
+  void addLocation(Location location) {
+    if (state is ActivityLocationLoaded) {
+      var updatedActivityLocations =
+          List<Location>.from(state.selectedLocations);
+      updatedActivityLocations.add(location);
+
+      state.filteredLocations.remove(location);
+      emit(ActivityLocationLoaded(
+          state.locations, updatedActivityLocations, state.filteredLocations));
+    }
+  }
+
+  void removeLocation(Location location) {
+    if (state is ActivityLocationLoaded) {
+      var updatedActivityLocations =
+          List<Location>.from(state.selectedLocations);
+      updatedActivityLocations.remove(location);
+      emit(ActivityLocationLoaded(
+          state.locations, updatedActivityLocations, state.filteredLocations));
+    }
+  }
+
+  void updateSelectedLocation(Location location) {
+    if (state is ActivityLocationLoaded) {
+      state.selectedLocations.clear();
+      var updatedActivityLocations =
+          List<Location>.from(state.selectedLocations);
+      updatedActivityLocations.add(location);
+
+      state.filteredLocations.remove(location); // ToDo: Test!
+      emit(ActivityLocationLoaded(
+          state.locations, updatedActivityLocations, state.filteredLocations));
+    }
+  }
+
+  Future<Activity> postActivityLocations() async {
+    emit(ActivityLocationPosting(state.locations, state.selectedLocations));
+
+    var currentActivity = await _activityRepository.getCurrentActivity();
+    var activity =
+        currentActivity.copyWith(location: state.selectedLocations[0]);
+    _activityRepository.updateLocalActivity(activity);
+
+    emit(ActivityLocationPosted(state.locations, state.selectedLocations));
+    return activity;
+  }
+}
