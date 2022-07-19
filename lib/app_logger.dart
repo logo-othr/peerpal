99034643581091a67eedd3897f41c154d2a35581
kreiterd <daniel.kreiter@gmx.de>
2022-07-19 20:55:49 +0200

    set colors to true

--- lib/app_logger.dart
+++ lib/app_logger.dart
@@ -1,3 +1,3 @@
 import 'package:logger/logger.dart';
 
-final logger = Logger(printer: PrefixPrinter(PrettyPrinter(colors: false)));
+final logger = Logger(printer: PrefixPrinter(PrettyPrinter(colors: true)));
