22c31d792bf8875a962866efa844fbea1545f1c9
kreiterd <daniel.kreiter@gmx.de>
2022-08-23 12:17:22 +0200

    refactor, code-cleanup

--- lib/profile_setup/presentation/name_input_page/view/name_input_content.dart
+++ lib/profile_setup/presentation/name_input_page/view/name_input_content.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:formz/formz.dart';
 import 'package:peerpal/peerpal_user/domain/peerpal_user.dart';
+import 'package:peerpal/profile_setup/domain/name_input_page/models/username_model.dart';
 import 'package:peerpal/profile_setup/presentation/name_input_page/cubit/name_input_cubit.dart';
 import 'package:peerpal/widgets/custom_peerpal_button.dart';
 import 'package:peerpal/widgets/custom_peerpal_heading.dart';
@@ -12,44 +13,84 @@ class NameInputContent extends StatelessWidget {
   final bool isInFlowContext;
   final String pastName;
 
-  NameInputContent(
+  const NameInputContent(
       {Key? key, required this.isInFlowContext, required this.pastName})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocListener<NameInputCubit, NameInputState>(
-      listener: (context, state) {
-        if (state.formValidationStatus.isSubmissionFailure) {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(
-                  content: Text((state.errorMessage.isEmpty
-                      ? 'Fehler beim Speichern.'
-                      : state.errorMessage))),
-            );
-        }
-      },
-      child: Align(
-        alignment: const Alignment(0, -1 / 3),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                const SizedBox(height: 16.0),
-                CustomPeerPALHeading1('Wie ist dein Name?'),
-                const SizedBox(height: 120),
-                _UsernameInputField(isInFlowContext, pastName),
-                const SizedBox(height: 190),
-                _NextButton(isInFlowContext),
-              ],
-            ),
-          ),
+      listener: _errorFeedbackForSavingName,
+      child: _NameInputUi(isInFlowContext: isInFlowContext, pastName: pastName),
+    );
+  }
+
+  void _errorFeedbackForSavingName(context, state) {
+    if (state.formValidationStatus.isSubmissionFailure) {
+      ScaffoldMessenger.of(context)
+        ..hideCurrentSnackBar()
+        ..showSnackBar(
+          SnackBar(
+              content: Text((state.errorMessage.isEmpty
+                  ? 'Fehler beim Speichern.'
+                  : state.errorMessage))),
+        );
+    }
+  }
+}
+
+class _NameInputUi extends StatelessWidget {
+  final isInFlowContext;
+  final pastName;
+
+  const _NameInputUi({
+    Key? key,
+    required this.isInFlowContext,
+    required this.pastName,
+  }) : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: const Alignment(0, -1 / 3),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+          child: _UiNameInputContent(
+              isInFlowContext: isInFlowContext, pastName: pastName),
         ),
       ),
+    );
+  }
+}
+
+class _UiNameInputContent extends StatelessWidget {
+  final isInFlowContext;
+  final pastName;
+
+  const _UiNameInputContent({
+    Key? key,
+    required this.isInFlowContext,
+    required this.pastName,
+  }) : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        const SizedBox(height: 16.0),
+        CustomPeerPALHeading1('Wie ist dein Name?'),
+        const SizedBox(height: 120),
+        _UsernameInputField(
+            isInFlowContext: isInFlowContext, pastName: pastName),
+        const SizedBox(height: 190),
+        _NextButton(isInFlowContext),
+      ],
     );
   }
 }
@@ -58,18 +99,24 @@ class _UsernameInputField extends StatelessWidget {
   final bool isInFlowContext;
   final String pastName;
 
-  _UsernameInputField(this.isInFlowContext, this.pastName);
+  const _UsernameInputField({
+    Key? key,
+    required this.isInFlowContext,
+    required this.pastName,
+  }) : super(
+          key: key,
+        );
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<NameInputCubit, NameInputState>(
-      buildWhen: (previous, current) => previous.username != current.username,
+      buildWhen: (previous, current) => _hasNameChanged(previous, current),
       builder: (context, state) {
-        return new FutureBuilder(
+        return FutureBuilder(
           future: context.read<NameInputCubit>().currentName(),
           initialData: pastName,
           builder: (BuildContext context, AsyncSnapshot<String?> text) {
-            return new TextFormField(
+            return TextFormField(
                 maxLength: 30,
                 initialValue: pastName,
                 style: const TextStyle(fontSize: 22),
@@ -81,15 +128,19 @@ class _UsernameInputField extends StatelessWidget {
                       : null,
                 ),
                 key: const Key('name_selection_username_field'),
-                onChanged: (username) {
-                  context.read<NameInputCubit>().nameChanged(username);
-                },
+                onChanged: (username) => _updateUserName(context, username),
                 keyboardType: TextInputType.name);
           },
         );
       },
     );
   }
+
+  void _updateUserName(BuildContext context, String username) =>
+      context.read<NameInputCubit>().nameChanged(username);
+
+  bool _hasNameChanged(NameInputState previous, NameInputState current) =>
+      previous.username != current.username;
 }
 
 class _NextButton extends StatelessWidget {
@@ -107,25 +158,32 @@ class _NextButton extends StatelessWidget {
             ? const CircularProgressIndicator()
             : CustomPeerPALButton(
                 onPressed: () async {
-                  if (state.formValidationStatus.isValidated) {
-                    await context.read<NameInputCubit>().postName();
-
-                    var selectedName = state.username;
-                    if (isInFlowContext) {
-                      context
-                          .flow<PeerPALUser>()
-                          .update((s) => s.copyWith(name: selectedName.value));
-                    } else {
-                      await context
-                          .read<NameInputCubit>()
-                          .updateNameAtServer(selectedName.value);
-                      Navigator.pop(context);
-                    }
-                  }
+                  _save(state, context);
                 },
                 text: isInFlowContext ? 'Weiter' : 'Speichern',
               );
       },
     );
+  }
+
+  Future<void> _save(NameInputState state, BuildContext context) async {
+    {
+      if (state.formValidationStatus.isValidated) {
+        await context.read<NameInputCubit>().postName();
+
+        var selectedName = state.username;
+        _switchPage(context, selectedName);
+      }
+    }
+  }
+
+  void _switchPage(BuildContext context, UsernameModel selectedName) {
+    if (isInFlowContext) {
+      context
+          .flow<PeerPALUser>()
+          .update((s) => s.copyWith(name: selectedName.value));
+    } else {
+      Navigator.pop(context);
+    }
   }
 }
