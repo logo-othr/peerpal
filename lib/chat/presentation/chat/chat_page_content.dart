8c444d05c6681e9c05714bf52072508e8608b76a
kreiterd <daniel.kreiter@gmx.de>
2022-09-12 15:53:36 +0200

    refactor: Improve the readability of the code

--- lib/chat/presentation/chat/chat_page_content.dart
+++ lib/chat/presentation/chat/chat_page_content.dart
@@ -1,11 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:peerpal/chat/domain/models/chat_message.dart';
 import 'package:peerpal/chat/domain/repository/chat_repository.dart';
+import 'package:peerpal/chat/domain/usecase_response/user_chat.dart';
 import 'package:peerpal/chat/presentation/chat/bloc/chat_page_bloc.dart';
+import 'package:peerpal/chat/presentation/chat/view/chat_message_input_field.dart';
 import 'package:peerpal/chat/presentation/user_detail_page/user_detail_page.dart';
 import 'package:peerpal/chat/single_chat_header_widget.dart';
 import 'package:peerpal/data/resources/colors.dart';
@@ -80,7 +81,6 @@ class ChatPageContent extends StatelessWidget {
       BuildContext context, WaitingForChatOrFirstMessage state) {
     return Column(
       children: [
-        // ToDo: verify that state cast is correctly used
         _chatHeaderBar(context, state.chatPartner),
         _FriendRequestButton(
             chatPartner: state.chatPartner,
@@ -88,7 +88,12 @@ class ChatPageContent extends StatelessWidget {
         Spacer(),
         ChatButtons(state.appUser.phoneNumber,
             textEditingController: textEditingController),
-        messageInputContainer(state.chatPartner, null, context),
+        ChatMessageInputField(
+          textEditingController: textEditingController,
+          focus: _focus,
+          sendTextMessage: () => sendChatMessage(state.chatPartner, null,
+              textEditingController.text, "0", context),
+        ),
       ],
     );
   }
@@ -112,47 +117,24 @@ class ChatPageContent extends StatelessWidget {
         _FriendRequestButton(
             chatPartner: state.chatPartner,
             appUserRepository: sl<AppUserRepository>()),
-        buildChatMessages(context, state),
-        (!state.userChat.chat.chatRequestAccepted &&
-                state.userChat.chat.startedBy != state.appUser.id)
-            ? _ChatRequestAcceptDenyButtons(context)
-            : StreamBuilder<int>(
-                stream: sl<ChatRepository>()
-                    .messageCountForChat(state.userChat.chat.chatId),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return CustomLoadingIndicator(text: "Lade Nachrichten..");
-                  } else if (snapshot.data == 0) {
-                    return Container(
-                      width: double.infinity,
-                      height: 500,
-                      alignment: Alignment.center,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Der Chat ist nicht mehr vorhanden."),
-                          SizedBox(
-                            height: 50,
-                          ),
-                          CustomPeerPALButton(
-                            text: "Zurück",
-                            onPressed: () => Navigator.pop(context),
-                          )
-                        ],
-                      ),
-                    );
-                  } else {
-                    return Column(
-                      children: [
-                        ChatButtons(state.appUser.phoneNumber,
-                            textEditingController: textEditingController),
-                        messageInputContainer(state.chatPartner,
-                            state.userChat.chat.chatId, context),
-                      ],
-                    );
-                  }
-                }),
+        _messages(context, state),
+        _ChatBottomBar(
+          appUser: state.appUser,
+          chatMessageController: textEditingController,
+          chatPartner: state.chatPartner,
+          chatMessageInputField: ChatMessageInputField(
+            textEditingController: textEditingController,
+            focus: _focus,
+            sendTextMessage: () => sendChatMessage(
+                state.chatPartner,
+                state.userChat.chat.chatId,
+                textEditingController.text,
+                "0",
+                context),
+          ),
+          userChat: state.userChat,
+          currentUserId: currentUserId,
+        ),
       ],
     );
   }
@@ -175,75 +157,6 @@ class ChatPageContent extends StatelessWidget {
     );
   }
 
-  Widget messageInputContainer(
-      PeerPALUser chatPartner, String? chatId, context) {
-    BoxDecoration messageInputContainerDecoration = BoxDecoration(
-        color: Colors.white,
-        border: Border(
-          top: BorderSide(
-            color: PeerPALAppColor.secondaryColor,
-            width: 1.0,
-          ),
-        ));
-
-    return Container(
-      decoration: messageInputContainerDecoration,
-      padding: const EdgeInsets.all(15),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: TextField(
-              style: TextStyle(fontSize: 22),
-              textInputAction: TextInputAction.newline,
-              keyboardType: TextInputType.multiline,
-              minLines: 1,
-              maxLines: 5,
-              focusNode: _focus,
-              onSubmitted: (value) {
-                sendChatMessage(chatPartner, chatId, textEditingController.text,
-                    "0", context);
-              },
-              controller: textEditingController,
-              textCapitalization: TextCapitalization.sentences,
-              autocorrect: true,
-              enableSuggestions: true,
-              decoration: InputDecoration(
-                contentPadding:
-                    const EdgeInsets.only(left: 20, top: 30, right: 20),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(
-                      color: PeerPALAppColor.primaryColor, width: 3.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(
-                      color: PeerPALAppColor.primaryColor, width: 3.0),
-                ),
-                filled: true,
-                fillColor: Colors.grey[100],
-                hintText: 'Nachricht',
-              ),
-            ),
-          ),
-          const SizedBox(width: 10),
-          Material(
-            color: Colors.white,
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: IconButton(
-                icon: const Icon(Icons.send, size: 35),
-                onPressed: () => sendChatMessage(chatPartner, chatId,
-                    textEditingController.text, "0", context),
-                color: PeerPALAppColor.primaryColor,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   Future<void> sendChatMessage(PeerPALUser chatPartner, String? chatId,
       String content, String type, BuildContext context) async {
     // type: 0 = text, 1 = image,
@@ -258,7 +171,7 @@ class ChatPageContent extends StatelessWidget {
     } else {}
   }
 
-  Widget buildChatMessages(BuildContext context, ChatLoaded state) {
+  Widget _messages(BuildContext context, ChatLoaded state) {
     return Flexible(
         child: StreamBuilder<List<ChatMessage>>(
       stream: state.messages,
@@ -334,7 +247,7 @@ class ChatPageContent extends StatelessWidget {
                   child: CircleAvatar(
                       backgroundColor: Colors.white,
                       radius: 20,
-                      child: (imageUrl!.isEmpty)
+                      child: (imageUrl.isEmpty)
                           ? Icon(
                               Icons.account_circle,
                               size: 40.0,
@@ -356,9 +269,96 @@ class ChatPageContent extends StatelessWidget {
       ],
     );
   }
+}
 
-  Widget _ChatRequestAcceptDenyButtons(BuildContext context) {
-    // ToDo: dispatch event instead of calling the repository manually
+class _ChatBottomBar extends StatefulWidget {
+  final String currentUserId;
+  final UserChat userChat;
+  final PeerPALUser appUser;
+  final PeerPALUser chatPartner;
+  final TextEditingController chatMessageController;
+  final ChatMessageInputField chatMessageInputField;
+
+  const _ChatBottomBar(
+      {required this.currentUserId,
+      required this.userChat,
+      required this.appUser,
+      required this.chatPartner,
+      required this.chatMessageController,
+      required this.chatMessageInputField,
+      Key? key})
+      : super(key: key);
+
+  @override
+  State<_ChatBottomBar> createState() => _ChatBottomBarState();
+}
+
+class _ChatBottomBarState extends State<_ChatBottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    if (isChatRequestNotAccepted && isChatNotStartedByAppUser) {
+      return _chatRequestReplyButtons(context, widget.currentUserId);
+    } else {
+      return _chatBottomBar();
+    }
+  }
+
+  Widget _chatBottomBar() {
+    return StreamBuilder<int>(
+      stream:
+          sl<ChatRepository>().messageCountForChat(widget.userChat.chat.chatId),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return CustomLoadingIndicator(text: "Chat wird geladen..");
+        } else if (snapshot.data == 0) {
+          return _chatDoesNotExist();
+        } else {
+          return _bottomBarContent();
+        }
+      },
+    );
+  }
+
+  bool get isChatNotStartedByAppUser =>
+      widget.userChat.chat.startedBy != widget.appUser.id;
+
+  bool get isChatRequestNotAccepted =>
+      !widget.userChat.chat.chatRequestAccepted;
+
+  Widget _bottomBarContent() {
+    return Column(
+      children: [
+        ChatButtons(widget.appUser.phoneNumber,
+            textEditingController: widget.chatMessageController),
+        widget.chatMessageInputField,
+      ],
+    );
+  }
+
+  Widget _chatDoesNotExist() {
+    return Container(
+      width: double.infinity,
+      height: 500,
+      alignment: Alignment.center,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Der Chat ist nicht mehr vorhanden."),
+          SizedBox(
+            height: 50,
+          ),
+          CustomPeerPALButton(
+            text: "Zurück",
+            onPressed: () => Navigator.pop(context),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _chatRequestReplyButtons(BuildContext context, String currentUserId) {
+    // ToDo: dispatch event?
     ChatPageBloc bloc = context.read<ChatPageBloc>();
     ChatLoaded currentState = bloc.state as ChatLoaded;
     return Padding(
