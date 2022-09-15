19034741ed38b70e2ea33f81d0f137244075eb2a
kreiterd <daniel.kreiter@gmx.de>
2022-09-15 16:00:34 +0200

    add: separate class for the friend request button

--- lib/chat/presentation/chat/chat_page_content.dart
+++ lib/chat/presentation/chat/chat_page_content.dart
@@ -5,6 +5,7 @@ import 'package:peerpal/chat/domain/repository/chat_repository.dart';
 import 'package:peerpal/chat/domain/usecase_response/user_chat.dart';
 import 'package:peerpal/chat/presentation/chat/bloc/chat_page_bloc.dart';
 import 'package:peerpal/chat/presentation/chat/view/chat_message_input_field.dart';
+import 'package:peerpal/chat/presentation/chat/view/friend_request_button.dart';
 import 'package:peerpal/chat/presentation/chat/view/message_list.dart';
 import 'package:peerpal/chat/presentation/user_detail_page/user_detail_page.dart';
 import 'package:peerpal/chat/single_chat_header_widget.dart';
@@ -14,8 +15,6 @@ import 'package:peerpal/setup.dart';
 import 'package:peerpal/widgets/chat_buttons.dart';
 import 'package:peerpal/widgets/custom_loading_indicator.dart';
 import 'package:peerpal/widgets/custom_peerpal_button.dart';
-import 'package:peerpal/widgets/single_chat_cancel_friend_request_button.dart';
-import 'package:peerpal/widgets/single_chat_send_friend_request_button.dart';
 import 'package:provider/provider.dart';
 
 class ChatPageContent extends StatelessWidget {
@@ -69,7 +68,7 @@ class ChatPageContent extends StatelessWidget {
     return Column(
       children: [
         _chatHeaderBar(context, state.chatPartner),
-        _FriendRequestButton(
+        FriendRequestButton(
             chatPartner: state.chatPartner,
             appUserRepository: sl<AppUserRepository>()),
         Spacer(),
@@ -90,7 +89,7 @@ class ChatPageContent extends StatelessWidget {
     return Column(
       children: [
         _chatHeaderBar(context, state.chatPartner),
-        _FriendRequestButton(
+        FriendRequestButton(
           chatPartner: state.chatPartner,
           appUserRepository: sl<AppUserRepository>(),
         ),
@@ -102,7 +101,7 @@ class ChatPageContent extends StatelessWidget {
     return Column(
       children: [
         _chatHeaderBar(context, state.chatPartner),
-        _FriendRequestButton(
+        FriendRequestButton(
             chatPartner: state.chatPartner,
             appUserRepository: sl<AppUserRepository>()),
         MessageList(state: state),
@@ -283,81 +282,3 @@ class _ChatBottomBarState extends State<_ChatBottomBar> {
   }
 }
 
-class _FriendRequestButton extends StatelessWidget {
-  final PeerPALUser chatPartner;
-  final AppUserRepository _appUserRepository;
-
-  const _FriendRequestButton(
-      {Key? key,
-      required this.chatPartner,
-      required AppUserRepository appUserRepository})
-      : this._appUserRepository = appUserRepository,
-        super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<List<PeerPALUser>>(
-        stream: _appUserRepository.getFriendList(),
-        builder:
-            (context, AsyncSnapshot<List<PeerPALUser>> friendListSnapshot) {
-          if (!friendListSnapshot.hasData ||
-              _chatPartnerIsAFriend(friendListSnapshot, chatPartner)) {
-            return Container();
-          } else {
-            return _friendRequestButton();
-          }
-        });
-  }
-
-  Widget _friendRequestButton() {
-    return StreamBuilder<List<PeerPALUser>>(
-        stream: _appUserRepository.getSentFriendRequestsFromUser(),
-        builder: (context,
-            AsyncSnapshot<List<PeerPALUser>> sentFriendRequestsSnapshot) {
-          if (sentFriendRequestsSnapshot.hasData) {
-            if (_friendRequestPending(
-                sentFriendRequestsSnapshot, chatPartner)) {
-              return _cancelFriendRequestButton();
-            } else {
-              return _sendFriendRequestButton();
-            }
-          } else {
-            return Container();
-          }
-        });
-  }
-
-  bool _chatPartnerIsAFriend(
-      AsyncSnapshot<List<PeerPALUser>> friendListSnapshot,
-      PeerPALUser chatPartner) {
-    return friendListSnapshot.data!
-        .map((e) => e.id)
-        .toList()
-        .contains(chatPartner.id);
-  }
-
-  bool _friendRequestPending(
-      AsyncSnapshot<List<PeerPALUser>> friendRequestsSnapshot,
-      PeerPALUser chatPartner) {
-    return friendRequestsSnapshot.data!
-        .map((e) => e.id)
-        .toList()
-        .contains(chatPartner.id);
-  }
-
-  Widget _sendFriendRequestButton() {
-    return SendFriendRequestButton(
-        buttonText: "Anfrage senden",
-        onPressed: () {
-          _appUserRepository.sendFriendRequestToUser(chatPartner);
-        });
-  }
-
-  Widget _cancelFriendRequestButton() {
-    return CancelFriendRequestButton(
-        buttonText: "Anfrage gesendet",
-        onPressed: () {
-          _appUserRepository.canceledFriendRequest(chatPartner);
-        });
-  }
-}
---
+++ lib/chat/presentation/chat/view/friend_request_button.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:peerpal/peerpal_user/data/repository/app_user_repository.dart';
+import 'package:peerpal/peerpal_user/domain/peerpal_user.dart';
+import 'package:peerpal/widgets/single_chat_cancel_friend_request_button.dart';
+import 'package:peerpal/widgets/single_chat_send_friend_request_button.dart';
+
+class FriendRequestButton extends StatelessWidget {
+  final PeerPALUser chatPartner;
+  final AppUserRepository _appUserRepository;
+
+  const FriendRequestButton(
+      {Key? key,
+      required this.chatPartner,
+      required AppUserRepository appUserRepository})
+      : this._appUserRepository = appUserRepository,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<PeerPALUser>>(
+        stream: _appUserRepository.getFriendList(),
+        builder:
+            (context, AsyncSnapshot<List<PeerPALUser>> friendListSnapshot) {
+          if (!friendListSnapshot.hasData ||
+              _chatPartnerIsAFriend(friendListSnapshot, chatPartner)) {
+            return Container();
+          } else {
+            return _friendRequestButton();
+          }
+        });
+  }
+
+  Widget _friendRequestButton() {
+    return StreamBuilder<List<PeerPALUser>>(
+        stream: _appUserRepository.getSentFriendRequestsFromUser(),
+        builder: (context,
+            AsyncSnapshot<List<PeerPALUser>> sentFriendRequestsSnapshot) {
+          if (sentFriendRequestsSnapshot.hasData) {
+            if (_friendRequestPending(
+                sentFriendRequestsSnapshot, chatPartner)) {
+              return _cancelFriendRequestButton();
+            } else {
+              return _sendFriendRequestButton();
+            }
+          } else {
+            return Container();
+          }
+        });
+  }
+
+  bool _chatPartnerIsAFriend(
+      AsyncSnapshot<List<PeerPALUser>> friendListSnapshot,
+      PeerPALUser chatPartner) {
+    return friendListSnapshot.data!
+        .map((e) => e.id)
+        .toList()
+        .contains(chatPartner.id);
+  }
+
+  bool _friendRequestPending(
+      AsyncSnapshot<List<PeerPALUser>> friendRequestsSnapshot,
+      PeerPALUser chatPartner) {
+    return friendRequestsSnapshot.data!
+        .map((e) => e.id)
+        .toList()
+        .contains(chatPartner.id);
+  }
+
+  Widget _sendFriendRequestButton() {
+    return SendFriendRequestButton(
+        buttonText: "Anfrage senden",
+        onPressed: () {
+          _appUserRepository.sendFriendRequestToUser(chatPartner);
+        });
+  }
+
+  Widget _cancelFriendRequestButton() {
+    return CancelFriendRequestButton(
+        buttonText: "Anfrage gesendet",
+        onPressed: () {
+          _appUserRepository.canceledFriendRequest(chatPartner);
+        });
+  }
+}
