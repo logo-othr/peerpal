0c7122c2b46fd9629782b00d171e5bced6042a80
kreiterd <daniel.kreiter@gmx.de>
2022-09-22 12:59:02 +0200

    ui changes

--- lib/settings/imprint_page.dart
+++ lib/settings/imprint_page.dart
@@ -17,60 +17,68 @@ class _ImprintPageState extends State<ImprintPage> {
         "Impressum",
         hasBackButton: true,
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              SizedBox(height: 20),
-              CustomPeerPALHeading3(
-                text: "Herausgeber",
-                color: PeerPALAppColor.primaryColor,
-              ),
-              SizedBox(height: 20),
-              Text(
-                  "Ostbayerische Technische Hochschule Regensburg (OTH), Seybothstr. 2, 93053 Regensburg"),
-              SizedBox(height: 20),
-              Text("und"),
-              SizedBox(height: 10),
-              Text(
-                  "Katholische Hochschule Mainz (KH), Saarstr. 3, 55122 Mainz"),
-              SizedBox(height: 10),
-              Text("Förderkennzeichen:"),
-              SizedBox(height: 10),
-              Text("13FH515SA7 – KH Mainz"),
-              Text("13FH515SB7 – OTH Regensburg"),
-              SizedBox(height: 20),
-              Text("Verantwortliche Personen"),
-              Column(
-                children: [
-                  Text("Frau Prof.in Dr. Norina Lauer"),
-                  Text("Tel.: +49 941 943-1087"),
-                  Text("Fax: +49 941 943-1468"),
-                  Text("E-Mail: norina.lauer@oth-regensburg.de "),
-                  Text("Ostbayerische Technische Hochschule Regensburg (OTH)"),
-                  Text("Seybothstr. 2"),
-                  Text("93053 Regensburg"),
-                ],
-              ),
-              SizedBox(height: 10),
-              Text("und"),
-              SizedBox(height: 10),
-              Column(
-                children: [
-                  Text("Frau Prof.in Dr. Sabine Corsten"),
-                  Text("Tel.: 06131 – 289 44 540"),
-                  Text("Fax: 06131 – 289 44 8 540"),
-                  Text("E-Mail: sabine.corsten@kh-mz.de "),
-                  Text("Katholische Hochschule Mainz (KH)"),
-                  Text("Saarstr. 3"),
-                  Text("55122 Main"),
-                ],
-              )
-            ],
-          ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 20),
+            CustomPeerPALHeading3(
+              text: "Herausgeber",
+              color: PeerPALAppColor.primaryColor,
+            ),
+            SizedBox(height: 20),
+            normalText("Ostbayerische Technische Hochschule Regensburg (OTH)"),
+            normalText("Seybothstr. 2"),
+            normalText("93053 Regensburg"),
+            SizedBox(height: 10),
+            Text("und"),
+            SizedBox(height: 10),
+            normalText("Katholische Hochschule Mainz (KH)"),
+            normalText("Saarstr. 3"),
+            normalText("55122 Mainz"),
+            SizedBox(height: 10),
+            CustomPeerPALHeading3(
+              text: "Förderkennzeichen",
+              color: PeerPALAppColor.primaryColor,
+            ),
+            SizedBox(height: 10),
+            normalText("13FH515SB7 – OTH Regensburg"),
+            normalText("13FH515SA7 – KH Mainz"),
+            SizedBox(height: 20),
+            CustomPeerPALHeading3(
+              text: "Verantwortliche Personen",
+              color: PeerPALAppColor.primaryColor,
+            ),
+            SizedBox(height: 20),
+            normalText("Frau Prof.in Dr. Norina Lauer"),
+            normalText("Tel.: +49 941 943-1087"),
+            normalText("Fax: +49 941 943-1468"),
+            normalText("E-Mail: norina.lauer@oth-regensburg.de "),
+            normalText("Ostbayerische Technische Hochschule Regensburg (OTH)"),
+            normalText("Seybothstr. 2"),
+            normalText("93053 Regensburg"),
+            SizedBox(height: 10),
+            Text("und"),
+            SizedBox(height: 10),
+            normalText("Frau Prof.in Dr. Sabine Corsten"),
+            normalText("Tel.: 06131 – 289 44 540"),
+            normalText("Fax: 06131 – 289 44 8 540"),
+            normalText("E-Mail: sabine.corsten@kh-mz.de "),
+            normalText("Katholische Hochschule Mainz (KH)"),
+            normalText("Saarstr. 3"),
+            normalText("55122 Main"),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget normalText(String text) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: 13),
     );
   }
 }
