a51744ca7b1f5766cc3b25758b3e3d2d785f0837
kreiterd <daniel.kreiter@gmx.de>
2022-09-22 12:05:13 +0200

    Enable back-button

--- lib/activity/presentation/activity_setup/activity_date/view/activity_date_input_page.dart
+++ lib/activity/presentation/activity_setup/activity_date/view/activity_date_input_page.dart
@@ -20,7 +20,7 @@ class ActivitySelectDatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: () async => false,
+      onWillPop: () async => true,
       child: BlocProvider(
         create: (_) {
           return DateInputCubit(context.read<ActivityRepository>())..load();
