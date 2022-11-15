4b67b8b168c7c704cf0ef623dda5b4bf8f25aa97
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-11-15 13:29:15 +0100

    add analytics_repository.dart

---
+++ lib/app/domain/analytics/analytics_repository.dart
@@ -0,0 +1,12 @@
+import 'package:peerpal/activity/domain/models/activity.dart';
+import 'package:peerpal/app/domain/support_videos/support_video.dart';
+
+abstract class AnalyticsRepository {
+  addVideoClick(String userId, String timestamp, SupportVideo supportVideo);
+
+  addUserSearch(String userId, String timestamp, String searchQuery);
+
+  addPublicActivityClick(String userId, String timestamp, Activity activity);
+
+  addAddActivityClick(String userId, String timestamp, String activityId);
+}
