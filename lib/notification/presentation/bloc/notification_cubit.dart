e3e2f31380667e1d00c94f9ebdc630201311525c
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-11-25 20:03:52 +0100

    add notification page cubit

---
+++ lib/notification/presentation/bloc/notification_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'notification_state.dart';
+
+class NotificationCubit extends Cubit<NotificationState> {
+  NotificationCubit() : super(NotificationInitial());
+}
