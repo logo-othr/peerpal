befdf3e94b98a200dd970f92a80bac156c146970
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-11-25 22:44:25 +0100

    update layout

--- lib/notification/presentation/notification_page_content.dart
+++ lib/notification/presentation/notification_page_content.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:peerpal/app_tab_view/domain/notification_service.dart';
+import 'package:peerpal/data/resources/colors.dart';
 import 'package:peerpal/setup.dart';
+import 'package:peerpal/widgets/custom_peerpal_button.dart';
 
 class NotificationPageContent extends StatefulWidget {
   @override
@@ -14,51 +16,65 @@ class _NotificationPageContentState extends State<NotificationPageContent> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-          child: Text(
-            "Bitte erlauben",
-            textAlign: TextAlign.justify,
-            style: TextStyle(
-                fontWeight: FontWeight.bold, height: 1.5, fontSize: 35),
-          ),
-        ),
-        SizedBox(
-          height: 50,
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-          child: Text(
-            "Damit wir dich über neue Benachrichtigungen informieren können, benötigen wir deine Erlaubnis. "
-            "Wenn du auf den Button klickst, dann bestätige bitte im nächsten Schritt mit 'Ja', damit du keine Erinnerungen verpasst.",
-            textAlign: TextAlign.justify,
-            style: TextStyle(
-                fontWeight: FontWeight.w600, height: 1.5, fontSize: 22),
-          ),
-        ),
-        SizedBox(
-          height: 50,
-        ),
-        ElevatedButton(
-            onPressed: () async {
-              NotificationService notificationService =
-                  sl<NotificationService>();
-              bool hasPermission =
-                  (await notificationService.requestPermission());
-              Navigator.pop(context);
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
               child: Text(
-                "Benachrichtigungen erlauben",
-                style: TextStyle(fontSize: 20),
+                "Bitte erlauben",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, height: 1.5, fontSize: 35),
               ),
-            ))
-      ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+              child: Text(
+                "Damit wir dich über neue Benachrichtigungen informieren können, benötigen wir deine Erlaubnis. "
+                "Wenn du auf den Button klickst, bestätige bitte im nächsten Schritt mit 'Erlauben', damit du keine Erinnerungen verpasst.",
+                style: TextStyle(
+                    fontWeight: FontWeight.w600, height: 1.5, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            CustomPeerPALButton(
+                text: 'Benachrichtigungen erlauben',
+                onPressed: () async {
+                  NotificationService notificationService =
+                      sl<NotificationService>();
+                  bool hasPermission =
+                      (await notificationService.requestPermission());
+                  Navigator.pop(context);
+                },
+                color: PeerPALAppColor.primaryColor),
+            /*ElevatedButton(
+                onPressed: () async {
+                  NotificationService notificationService =
+                      sl<NotificationService>();
+                  bool hasPermission =
+                      (await notificationService.requestPermission());
+                  if (hasPermission) Navigator.pop(context);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Benachrichtigungen erlauben",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ))*/
+          ],
+        ),
+      ),
     );
     /*return BlocBuilder<NotificationCubit, NotificationState>(
         builder: (context, state) {
