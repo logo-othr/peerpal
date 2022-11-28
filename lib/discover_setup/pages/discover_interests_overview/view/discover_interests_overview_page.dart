40b96580b7d51246e7bc88728fde8c141ca0de70
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-11-28 03:26:01 +0100

    add support video dialog

--- lib/discover_setup/pages/discover_interests_overview/view/discover_interests_overview_page.dart
+++ lib/discover_setup/pages/discover_interests_overview/view/discover_interests_overview_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/app/domain/support_videos/support_video_enum.dart';
+import 'package:peerpal/data/resources/support_video_links.dart';
 import 'package:peerpal/discover_feed/data/repository/app_user_repository.dart';
 import 'package:peerpal/discover_setup/pages/discover_communication/domain/get_user_usecase.dart';
 import 'package:peerpal/discover_setup/pages/discover_interests_overview/cubit/discover_interests_overview_cubit.dart';
 import 'package:peerpal/discover_setup/pages/discover_interests_overview/view/discover_interests_overview_content.dart';
 import 'package:peerpal/setup.dart';
 import 'package:peerpal/widgets/custom_app_bar.dart';
+import 'package:peerpal/widgets/support_video_dialog.dart';
 
 class DiscoverInterestsOverviewPage extends StatelessWidget {
   static MaterialPage<void> page() {
@@ -18,7 +21,11 @@ class DiscoverInterestsOverviewPage extends StatelessWidget {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
-        appBar: CustomAppBar("Überblick", hasBackButton: false),
+        appBar: CustomAppBar("Überblick",
+            hasBackButton: false,
+            actionButtonWidget: CustomSupportVideoDialog(
+                supportVideo: SupportVideos
+                    .links[VideoIdentifier.settings_profile_tab]!)),
         body: BlocProvider.value(
           value: DiscoverInterestsOverviewCubit(
               context.read<AppUserRepository>(), sl<GetAuthenticatedUser>())
