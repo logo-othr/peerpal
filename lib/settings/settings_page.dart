d8e272eee583e77decd6dcdb70a7400d862a3914
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-11-28 03:38:42 +0100

    first draft developer settings dialog

--- lib/settings/settings_page.dart
+++ lib/settings/settings_page.dart
@@ -16,12 +16,15 @@ import 'package:peerpal/settings/privacy_policy_page.dart';
 import 'package:peerpal/setup.dart';
 import 'package:peerpal/widgets/custom_app_bar.dart';
 import 'package:peerpal/widgets/custom_dialog.dart';
+import 'package:peerpal/widgets/custom_peerpal_button.dart';
+import 'package:peerpal/widgets/custom_peerpal_heading.dart';
 import 'package:peerpal/widgets/custom_table_header.dart';
 import 'package:peerpal/widgets/custom_table_row.dart';
 import 'package:peerpal/widgets/support_video_dialog.dart';
 import 'package:provider/src/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:timezone/timezone.dart' as tz;
+
 class SettingsPage extends StatefulWidget {
   @override
   _SettingsPageState createState() => _SettingsPageState();
@@ -44,6 +47,10 @@ class _SettingsPageState extends State<SettingsPage> {
     String buildNumber = packageInfo.buildNumber;
     return "[App-Name: ${appName}, Package-Name: ${packageName}, Version: ${version}, Build-Number: ${buildNumber}] ";
   }*/
+
+  bool isSwitched = false;
+  String password = "";
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -177,79 +184,256 @@ class _SettingsPageState extends State<SettingsPage> {
                   ),
                 ],
               ),
-              Column(
-                children: [
-                  CustomTableHeader(heading: "Entwickler-Einstellungen"),
-                  CustomTableRow(
-                    text:
-                        "ID: ${sl.get<AuthenticationRepository>().currentUser.id}",
-                    isArrowVisible: false,
-                  ),
-                  CustomTableRow(
-                    text: "Push-Notification-Test",
-                    onPressed: () async => {
-                      sl<NotificationService>().scheduleNotification(
-                          "test",
-                          "test",
-                          tz.TZDateTime.now(tz.local).add(Duration(seconds: 5)))
-                    },
+              Container(
+                // ToDo: refactor!
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border(
+                        bottom: BorderSide(
+                            width: 1, color: PeerPALAppColor.secondaryColor))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(18, 0, 0, 0),
+                      child: CustomPeerPALHeading3(
+                        text: "Entwickler-Einstellungen",
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                      child: Switch(
+                        value: isSwitched,
+                        onChanged: (value) {
+                          setState(() {
+                            isSwitched = value;
+                          });
+                          if (isSwitched == true) {
+                            showDialog(
+                                barrierDismissible: false,
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return Dialog(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    elevation: 0,
+                                    backgroundColor: Colors.white,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          border: Border.all(
+                                              width: 2,
+                                              color:
+                                                  PeerPALAppColor.primaryColor),
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      height: 300,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: <Widget>[
+                                          Text(
+                                            "Entwicklereinstellungen \n anschalten?",
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          SizedBox(
+                                            height: 20,
+                                          ),
+                                          Align(
+                                            alignment: Alignment.center,
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                      0, 10, 0, 0),
+                                              child: Container(
+                                                color: Colors.transparent,
+                                                child: Column(
+                                                  children: [
+                                                    Padding(
+                                                      padding: const EdgeInsets
+                                                              .fromLTRB(
+                                                          15, 10, 0, 10),
+                                                      child: CustomPeerPALHeading3(
+                                                          text:
+                                                              "Passwort eingeben:",
+                                                          fontSize: 20,
+                                                          color: Colors.black),
+                                                    ),
+                                                    Padding(
+                                                      padding: const EdgeInsets
+                                                              .fromLTRB(
+                                                          15, 5, 15, 0),
+                                                      child: TextField(
+                                                        textInputAction:
+                                                            TextInputAction
+                                                                .newline,
+                                                        keyboardType:
+                                                            TextInputType
+                                                                .multiline,
+                                                        minLines: 1,
+                                                        maxLines: 5,
+                                                        style: TextStyle(
+                                                            fontSize: 18),
+                                                        controller:
+                                                            passwordController,
+                                                        decoration:
+                                                            InputDecoration(
+                                                          contentPadding:
+                                                              const EdgeInsets
+                                                                      .only(
+                                                                  left: 35,
+                                                                  top: 35,
+                                                                  right: 35),
+                                                          hintMaxLines: 3,
+                                                          filled: true,
+                                                          fillColor:
+                                                              Colors.white,
+                                                          border:
+                                                              OutlineInputBorder(
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        10),
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 30,
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.fromLTRB(
+                                                20.0, 0, 20, 0),
+                                            child: CustomPeerPALButton(
+                                                onPressed: () {
+                                                  setState(() {
+                                                    password =
+                                                        passwordController.text;
+                                                    if (password == "1234") {
+                                                      isSwitched = true;
+                                                    } else {
+                                                      isSwitched = false;
+                                                    }
+                                                  });
+                                                  Navigator.pop(context);
+                                                },
+                                                text: "Ok"),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                });
+                          } else {
+                            setState(() {
+                              password = "";
+                            });
+                          }
+                        },
+                        activeTrackColor: PeerPALAppColor.secondaryColor,
+                        activeColor: PeerPALAppColor.primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              password == "1234"
+                  ? Column(
+                      children: [
+                        CustomTableRow(
+                          text:
+                              "ID: ${sl.get<AuthenticationRepository>().currentUser.id}",
+                          isArrowVisible: false,
+                        ),
+                        CustomTableRow(
+                          text: "Push-Notification-Test",
+                          onPressed: () async => {
+                            sl<NotificationService>().scheduleNotification(
+                                "test",
+                                "test",
+                                tz.TZDateTime.now(tz.local)
+                                    .add(Duration(seconds: 5)))
+                          },
                   ),
                   CustomTableRow(
                     text: "print push notification",
-                    onPressed: () async =>
-                        {sl<NotificationService>().printPendingNotifications()},
-                  ),
+                          onPressed: () async => {
+                            sl<NotificationService>()
+                                .printPendingNotifications()
+                          },
+                        ),
                   CustomTableRow(
                     text: "schedule weekly reminder",
-                    onPressed: () async => {
-                      sl<NotificationService>().scheduleWeeklyNotification()
-                    },
+                    onPressed: () async =>
+                    {
+                            sl<NotificationService>()
+                                .scheduleWeeklyNotification()
+                          },
                   ),
                   CustomTableRow(
-                    text: "reset pending notifications and set weekly reminder",
-                    onPressed: () async {
-                      sl<NotificationService>().cancelAll();
-                      sl<NotificationService>()
-                          .setWeeklyReminderScheduled(false);
-                      sl<NotificationService>().scheduleWeeklyNotification();
-                    },
-                  ),
+                          text:
+                              "reset pending notifications and set weekly reminder",
+                          onPressed: () async {
+                            sl<NotificationService>().cancelAll();
+                            sl<NotificationService>()
+                                .setWeeklyReminderScheduled(false);
+                            sl<NotificationService>()
+                                .scheduleWeeklyNotification();
+                          },
+                        ),
                   CustomTableRow(
-                    text: "reset pending notifications and set daily reminder",
-                    onPressed: () async {
-                      sl<NotificationService>().cancelAll();
-                      sl<NotificationService>()
-                          .setWeeklyReminderScheduled(false);
-                      sl<NotificationService>().scheduleDailyNotification();
-                    },
-                  ),
+                          text:
+                              "reset pending notifications and set daily reminder",
+                          onPressed: () async {
+                            sl<NotificationService>().cancelAll();
+                            sl<NotificationService>()
+                                .setWeeklyReminderScheduled(false);
+                            sl<NotificationService>()
+                                .scheduleDailyNotification();
+                          },
+                        ),
                   CustomTableRow(
                     text: "reset pending notifications",
-                    onPressed: () async =>
-                        {sl<NotificationService>().cancelAll()},
-                  ),
+                          onPressed: () async =>
+                              {sl<NotificationService>().cancelAll()},
+                        ),
                   CustomTableRow(
                     text: "print shared prefs",
                     onPressed: () async {
                       SharedPreferences prefs =
-                          await SharedPreferences.getInstance();
+                                await SharedPreferences.getInstance();
 
-                      for (String key in prefs.getKeys()) {
-                        logger.i(
-                            "key: ${key} value: ${prefs.get(key).toString()}");
-                      }
-                    },
-                  ),
-                  CustomTableRow(
-                    text: "clear shared prefs",
-                    onPressed: () async {
-                      SharedPreferences prefs =
-                          await SharedPreferences.getInstance();
-                      await prefs.clear();
-                    },
-                  ),
-                ],
-              ),
+                            for (String key in prefs.getKeys()) {
+                              logger.i(
+                                  "key: ${key} value: ${prefs.get(key).toString()}");
+                            }
+                          },
+                        ),
+                        CustomTableRow(
+                          text: "clear shared prefs",
+                          onPressed: () async {
+                            SharedPreferences prefs =
+                                await SharedPreferences.getInstance();
+                            await prefs.clear();
+                          },
+                        ),
+                      ],
+                    )
+                  : Container(),
             ],
           ),
         ),
