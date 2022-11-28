b66937d4371e485d0d72a4ab8f84e22362941190
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-11-28 03:26:15 +0100

    add support video dialog

--- lib/authentication/presentation/login/view/login_page.dart
+++ lib/authentication/presentation/login/view/login_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/app/domain/support_videos/support_video_enum.dart';
 import 'package:peerpal/authentication/persistence/authentication_repository.dart';
 import 'package:peerpal/authentication/presentation/presentation.dart';
+import 'package:peerpal/data/resources/support_video_links.dart';
 import 'package:peerpal/widgets/custom_app_bar.dart';
+import 'package:peerpal/widgets/support_video_dialog.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,10 +15,11 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        'PeerPAL',
-        hasBackButton: false,
-      ),
+      appBar: CustomAppBar('PeerPAL',
+          hasBackButton: false,
+          actionButtonWidget: CustomSupportVideoDialog(
+              supportVideo:
+                  SupportVideos.links[VideoIdentifier.settings_profile_tab]!)),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: BlocProvider(
