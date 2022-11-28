20d503f79fceaab37a9a3fd52c1e7d56215975a0
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-11-28 03:26:42 +0100

    add support video dialog

--- lib/app/domain/support_videos/support_video_enum.dart
+++ lib/app/domain/support_videos/support_video_enum.dart
@@ -1,1 +1,20 @@
-enum VideoIdentifier { discover_tab }
+import 'package:json_annotation/json_annotation.dart';
+
+enum VideoIdentifier {
+  @JsonValue("discover_tab")
+  discover_tab,
+  @JsonValue("activity_tab")
+  activity_tab,
+  @JsonValue("chat_tab")
+  chat_tab,
+  @JsonValue("settings_tab")
+  settings_tab,
+  @JsonValue("friends_tab")
+  friends_tab,
+  @JsonValue("chat_request_tab")
+  chat_request_tab,
+  @JsonValue("activity_invitation_tab")
+  activity_invitation_tab,
+  @JsonValue("settings_profile_tab")
+  settings_profile_tab
+}
