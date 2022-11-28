47849ee7f51378e56503792c1511c047174cc073
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-11-28 03:26:18 +0100

    add support video dialog

--- lib/profile_setup/presentation/name_input_page/view/name_input_page.dart
+++ lib/profile_setup/presentation/name_input_page/view/name_input_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/app/domain/support_videos/support_video_enum.dart';
+import 'package:peerpal/data/resources/support_video_links.dart';
 import 'package:peerpal/discover_feed/data/repository/app_user_repository.dart';
 import 'package:peerpal/discover_setup/pages/discover_communication/domain/get_user_usecase.dart';
 import 'package:peerpal/profile_setup/presentation/name_input_page/cubit/name_input_cubit.dart';
 import 'package:peerpal/profile_setup/presentation/name_input_page/view/name_input_content.dart';
 import 'package:peerpal/setup.dart';
 import 'package:peerpal/widgets/custom_app_bar.dart';
+import 'package:peerpal/widgets/support_video_dialog.dart';
 
 class NameInputPage extends StatelessWidget {
   final bool isInFlowContext;
@@ -24,10 +27,11 @@ class NameInputPage extends StatelessWidget {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
-        appBar: CustomAppBar(
-          'PeerPAL',
-          hasBackButton: true,
-        ),
+        appBar: CustomAppBar('PeerPAL',
+            hasBackButton: true,
+            actionButtonWidget: CustomSupportVideoDialog(
+                supportVideo: SupportVideos
+                    .links[VideoIdentifier.settings_profile_tab]!)),
         body: BlocProvider(
           create: (_) {
             return NameInputCubit(
