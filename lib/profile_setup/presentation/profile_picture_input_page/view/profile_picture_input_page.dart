46130b075861c11e51922bbb0f5684cd542f4386
Daniel Kreiter <daniel.kreiter@gmx.de>
2022-11-28 03:26:30 +0100

    add support video dialog

--- lib/profile_setup/presentation/profile_picture_input_page/view/profile_picture_input_page.dart
+++ lib/profile_setup/presentation/profile_picture_input_page/view/profile_picture_input_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/app/domain/support_videos/support_video_enum.dart';
 import 'package:peerpal/authentication/persistence/authentication_repository.dart';
+import 'package:peerpal/data/resources/support_video_links.dart';
 import 'package:peerpal/discover_feed/data/repository/app_user_repository.dart';
 import 'package:peerpal/discover_setup/pages/discover_communication/domain/get_user_usecase.dart';
 import 'package:peerpal/profile_setup/presentation/profile_picture_input_page/cubit/profile_picture_cubit.dart';
 import 'package:peerpal/profile_setup/presentation/profile_picture_input_page/view/profile_picture_input_content.dart';
 import 'package:peerpal/setup.dart';
 import 'package:peerpal/widgets/custom_app_bar.dart';
+import 'package:peerpal/widgets/support_video_dialog.dart';
 
 class ProfilePictureInputPage extends StatelessWidget {
   final bool isInFlowContext;
@@ -24,10 +27,11 @@ class ProfilePictureInputPage extends StatelessWidget {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
-        appBar: CustomAppBar(
-          "PeerPAL",
-          hasBackButton: true,
-        ),
+        appBar: CustomAppBar("PeerPAL",
+            hasBackButton: true,
+            actionButtonWidget: CustomSupportVideoDialog(
+                supportVideo: SupportVideos
+                    .links[VideoIdentifier.settings_profile_tab]!)),
         body: BlocProvider(
           create: (_) {
             return ProfilePictureCubit(
