2fa8663fee30abff37d894b2441f32517d600d60
Daniel Kreiter <daniel.kreiter@gmx.de>
2023-05-10 17:23:01 +0200

    Doc: Add method description

--- lib/activity/domain/repository/activity_reminder_repository.dart
+++ lib/activity/domain/repository/activity_reminder_repository.dart
@@ -1,7 +1,11 @@
 import 'package:peerpal/activity/domain/models/activity.dart';
 
 abstract class ActivityReminderRepository {
+  /// Deletes all existing reminders associated with the activities that the
+  /// current user has joined and sets new reminders for the provided [activities].
   Future<void> setJoinedActivitiesReminders(List<Activity> activities);
 
+  /// Deletes all existing reminders associated with the activities that the
+  /// current user has created and sets new reminders for the provided [activities].
   Future<void> setCreatedActivitiesReminders(List<Activity> activities);
 }
