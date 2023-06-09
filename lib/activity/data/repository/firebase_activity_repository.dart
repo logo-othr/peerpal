52e124ec124b47098026f8797ce495e026398b87
Daniel Kreiter <daniel.kreiter@gmx.de>
2023-06-09 15:02:38 +0200

    remove sortActivityList from firebase activity repository

--- lib/activity/data/repository/firebase_activity_repository.dart
+++ lib/activity/data/repository/firebase_activity_repository.dart
@@ -193,36 +193,6 @@ class FirebaseActivityRepository implements ActivityRepository {
     return activityList;
   }
 
-  /// Sorts a list of [Activity] objects based on the creator and activity date.
-  ///
-  /// The sortActivityList method takes in a list of [Activity] objects (listToSort) and the ID
-  /// of the currently logged-in user (currentUserId). It separates the activities created by the
-  /// current user into a creatorList and other public activities into a publicList. Both lists
-  /// are sorted in descending order based on the activity date. The sorted lists are combined and
-  /// returned, with the activities created by the current user first.
-  ///
-  /// Returns a new [List] of sorted [Activity] objects.
-  List<Activity> sortActivityList(List<Activity> list, String currentUserId) {
-    List<Activity> creatorList = [];
-    List<Activity> publicList = [];
-    List<Activity> sortedList = [];
-    for (Activity activity in list) {
-      if (activity.creatorId == currentUserId)
-        creatorList.add(activity);
-      else
-        publicList.add(activity);
-    }
-
-    // Sort by date
-    creatorList.sort((a, b) => b.date!.compareTo(a.date!));
-    publicList.sort((a, b) => b.date!.compareTo(a.date!));
-
-    // Combine
-    sortedList.addAll(creatorList);
-    sortedList.addAll(publicList);
-
-    return sortedList;
-  }
 
   /// The getJoinActivityRequests method returns a real-time stream of lists, each containing
   /// [Activity] objects for which the current user has received an invitation and the date is in the future.
