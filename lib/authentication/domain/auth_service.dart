d77f95e2d68a5229e4f49a9e79feb75f9b819962
Daniel Kreiter <daniel.kreiter@gmx.de>
2023-06-19 19:59:43 +0200

    Refactor: Moved methods from Authentication Repository to AuthService interface and implemented them in FirebaseAuthService

--- lib/authentication/domain/auth_service.dart
+++ lib/authentication/domain/auth_service.dart
@@ -1,3 +1,18 @@
+import 'package:peerpal/authentication/domain/models/auth_user.dart';
+
 abstract class AuthService {
   Future<String> getCurrentUserId();
+
+  Stream<AuthUser> get user;
+
+  Future<void> signUp({required String email, required String password});
+
+  Future<void> loginWithEmailAndPassword(
+      {required String email, required String password});
+
+  Future<void> logout();
+
+  Future<bool> resetPassword({required String email});
+
+  AuthUser get currentUser;
 }
--- lib/authentication/persistence/authentication_repository.dart
+++ lib/authentication/persistence/authentication_repository.dart
@@ -1,145 +1,52 @@
-import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:peerpal/app/domain/core/cache.dart';
-import 'package:peerpal/app_logger.dart';
+import 'package:peerpal/authentication/domain/auth_service.dart';
 import 'package:peerpal/authentication/domain/models/auth_user.dart';
 
-class SignUpFailure implements Exception {
-  SignUpFailure({this.message = 'Fehler bei der Registierung'});
-
-  final String message;
-}
-
-class LoginException implements Exception {
-  LoginException({this.message = 'Fehler beim Login.'});
-
-  final String message;
-}
-
-class LogoutException implements Exception {
-  LogoutException({this.message = 'Fehler beim Ausloggen.'});
-
-  final String message;
-}
-
 class AuthenticationRepository {
-  final firebase_auth.FirebaseAuth _firebaseAuth;
+  final AuthService _authService;
   final Cache cache;
 
-  AuthenticationRepository({
-    firebase_auth.FirebaseAuth? firebaseAuth,
-    required this.cache,
-  }) : _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance;
+  AuthenticationRepository(
+      {required AuthService authService, required this.cache})
+      : _authService = authService;
 
-  Stream<AuthUser> get user {
-    return _firebaseAuth.authStateChanges().map((firebaseUser) {
-      return firebaseUser == null ? AuthUser.empty : _getUserFromFirebaseUser();
-    });
+  Stream<AuthUser> get user => _authService.user;
+
+  AuthUser get currentUser => _authService.currentUser;
+
+  Future<void> signUp({required String email, required String password}) async {
+    try {
+      await _authService.signUp(email: email, password: password);
+    } catch (e) {
+      throw e;
+    }
   }
 
-  AuthUser get currentUser {
-    return _getUserFromFirebaseUser();
-  }
-
-  AuthUser _getUserFromFirebaseUser() {
-    var firebaseUser = firebase_auth.FirebaseAuth.instance.currentUser;
-
-    return (firebaseUser == null
-        ? AuthUser.empty
-        : AuthUser(id: firebaseUser.uid, email: firebaseUser.email));
+  Future<void> loginWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      await _authService.loginWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      throw e;
+    }
   }
 
   Future<bool> resetPassword({required String email}) async {
     try {
-      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
-      logger.i("password reset email sent");
-      return true;
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        logger.i('No user found for that email.');
-        // ToDo: throw exception
-        return false;
-      }
+      return await _authService.resetPassword(email: email);
+    } catch (e) {
+      throw e;
     }
-    return false;
-  }
-
-  Future<void> signUp({required String email, required String password}) async {
-    try {
-      await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-    } on FirebaseAuthException catch (e) {
-      // https://pub.dev/documentation/firebase_auth/latest/firebase_auth/FirebaseAuth/createUserWithEmailAndPassword.html
-      switch (e.code) {
-        case 'email-already-in-use':
-          throw SignUpFailure(
-              message: "Diese E-Mail ist bereits in Benutzung.");
-        case 'operation-not-allowed':
-          throw SignUpFailure(
-              message: 'Es ist ein Fehler aufgetreten. '
-                  'Registierungen sind deaktiviert.');
-        case 'weak-password:':
-          throw SignUpFailure(message: 'Das gewählte Passwort ist zu schwach.');
-        case 'too-many-requests':
-          throw SignUpFailure(
-              message: 'Der Server ist ausgelastet. Bitte versuche es später '
-                  'oder morgen noch einmal.');
-        default:
-          throw SignUpFailure();
-      }
-    } on Exception {
-      throw SignUpFailure();
-    }
-  }
-
-  Future<void> loginWithEmailAndPassword({
-    required String email,
-    required String password,
-  }) async {
-    try {
-      await _firebaseAuth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-    } on FirebaseAuthException catch (e) {
-      // error-codes: https://pub.dev/documentation/firebase_auth/latest/firebase_auth/FirebaseAuth/signInWithEmailAndPassword.html
-      switch (e.code) {
-        case 'wrong-password':
-        case 'user-not-found':
-          throw LoginException(
-              message:
-                  'Falsches Passwort oder die der Nutzer existiert nicht.');
-        case 'user-disabled':
-          throw LoginException(message: 'Der Account wurde deaktiviert.');
-        case 'invalid-email':
-          throw LoginException(message: 'Die E-Mail ist ungültig.');
-        case 'too-many-requests':
-          throw LoginException(
-              message:
-                  'Der Server ist ausgelastet. Bitte versuche es später oder '
-                  "morgen noch einmal.");
-        default:
-          throw LoginException();
-      }
-    } on Exception {
-      throw LoginException();
-    }
-
-    // await registerFCMDeviceToken(); // disabled because we want to register the device token only after the profile and discover setup ToDo: Test.
   }
 
   Future<void> logout() async {
     try {
-      await Future.wait([
-        _firebaseAuth.signOut(),
-      ]);
-      cache.clearCache(key: '{$currentUser.uid}-userinformation');
-    } on Exception {
-      throw LogoutException();
+      await _authService.logout();
+      // TODO: Check if the cache is used somewhere?
+      //  cache.clearCache(key: '${currentUser.id}-userinformation');
+    } catch (e) {
+      throw e;
     }
   }
-
-
 }
