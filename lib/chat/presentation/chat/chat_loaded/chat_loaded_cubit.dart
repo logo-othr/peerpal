8ebb07606bde117339c52c4417d032332af8765e
Daniel Kreiter <daniel.kreiter@gmx.de>
2023-07-11 14:15:03 +0200

    Implement sendMessage()

--- lib/chat/presentation/chat/chat_loaded/chat_loaded_cubit.dart
+++ lib/chat/presentation/chat/chat_loaded/chat_loaded_cubit.dart
@@ -1,61 +1,30 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/chat/domain/message_type.dart';
+import 'package:peerpal/chat/domain/usecases/send_chat_message_usecase.dart';
+import 'package:peerpal/discover_feed/domain/peerpal_user.dart';
+
 part 'chat_loaded_state.dart';
 
-/*
 class ChatLoadedCubit extends Cubit<ChatLoadedBasicState> {
-  final SendChatMessageUseCase sendMessageUseCase;
+  final SendChatMessageUseCase _sendMessage;
 
   ChatLoadedCubit({
-    required this.sendMessageUseCase,
-  }) : super(InitialChatLoadedState());
+    required sendMessage,
+  })  : this._sendMessage = sendMessage,
+        super(InitialChatLoadedState());
 
-  Future<void> sendTextMessage(PeerPALUser chatPartner, String? chatId,
-      String content, BuildContext context) async {
-  //  _messagingService.sendTextMessage(chatPartner, chatId, content, context);
-    emit(state.copyWith());
-  }
-
-  Future<void> sendImageMessage(PeerPALUser chatPartner, String? chatId,
-      String content, BuildContext context) async {
-    var image = await pickPictureFromGallery();
-    String url = await postPicture(image, _state.userChat);
-    _messagingService.sendImageMessage(chatPartner, chatId, url, context);
-    emit(state.copyWith());
-  }
-
-  Future<XFile> pickPictureFromGallery() async {
-    var profilePicture = (await ImagePicker().pickImage(
-      source: ImageSource.gallery,
-      maxHeight: 1280,
-      maxWidth: 720,
-      imageQuality: 45,
-    ))!;
-    return profilePicture;
-  }
-
-  Future<String> postPicture(XFile? chatImage, UserChat? userChat) async {
-    var uid = Uuid();
-
-    firebase_storage.UploadTask uploadTask;
-    var ref = firebase_storage.FirebaseStorage.instance
-        .ref()
-        .child('User-Chat-Image')
-        .child(userChat!.chat.chatId)
-        .child(DateTime.now().millisecondsSinceEpoch.toString())
-        .child('${uid.v4()}.jpg');
-
-    final metadata = firebase_storage.SettableMetadata(
-        contentType: 'image/jpeg',
-        customMetadata: {'file-path': chatImage!.path});
-
-    uploadTask = ref.putFile(File(chatImage.path), metadata);
-
-    var returnURL = '';
-    await Future.value(uploadTask);
-    await ref.getDownloadURL().then((fileURL) {
-      returnURL = fileURL;
-    });
-    logger.i(returnURL);
-    return returnURL;
+  Future<void> sendMessage(
+      {required PeerPALUser chatPartner,
+      required String? chatId,
+      required String payload,
+      required MessageType messageType}) async {
+    if (payload.trim() != '') {
+      await _sendMessage(
+        chatPartner,
+        chatId,
+        payload,
+        messageType,
+      );
+    }
   }
 }
-*/
