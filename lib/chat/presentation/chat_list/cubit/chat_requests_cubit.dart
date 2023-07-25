cb79b363dfa3f3ab6bf36ae0705456b1a39bc354
Daniel Kreiter <daniel.kreiter@gmx.de>
2023-07-25 16:22:18 +0200

    Implement ChatRequestsCubit

---
+++ lib/chat/presentation/chat_list/cubit/chat_requests_cubit.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:peerpal/authentication/persistence/authentication_repository.dart';
+import 'package:peerpal/chat/domain/usecase_response/user_chat.dart';
+import 'package:peerpal/chat/domain/usecases/get_all_userchats.dart';
+
+part 'chat_requests_state.dart';
+
+class ChatRequestsCubit extends Cubit<ChatRequestsState> {
+  final GetAllUserChats _getAllUserChats;
+  final AuthenticationRepository _authenticationRepository;
+  StreamSubscription<List<UserChat>>? _requestsSubscription;
+
+  ChatRequestsCubit(this._getAllUserChats, this._authenticationRepository)
+      : super(ChatRequestsState());
+
+  void loadChatRequests() async {
+    final userChatsStream = _getAllUserChats();
+    String userId = _authenticationRepository.currentUser.id;
+    _requestsSubscription = userChatsStream.listen((userChats) {
+      final filteredUserChats = userChats.where((userChat) {
+        return userChat.chat.startedBy != userId &&
+            !userChat.chat.chatRequestAccepted;
+      }).toList();
+
+      emit(state.copyWith(
+          status: ChatRequestsStatus.success, requests: filteredUserChats));
+    });
+  }
+
+  @override
+  Future<void> close() {
+    _requestsSubscription?.cancel();
+    return super.close();
+  }
+}
