53646e3d07595571f92152f2773977542620628d
Daniel Kreiter <daniel.kreiter@gmx.de>
2023-07-28 15:26:02 +0200

    Use ChatPageCubit instead of ChatPageBloc

--- lib/chat/presentation/chat/chat_page/view/chat_page.dart
+++ lib/chat/presentation/chat/chat_page/view/chat_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:peerpal/authentication/persistence/authentication_repository.dart';
 import 'package:peerpal/chat/domain/repository/chat_repository.dart';
 import 'package:peerpal/chat/domain/usecase_response/user_chat.dart';
-import 'package:peerpal/chat/domain/usecases/get_chat_messages_usecase.dart';
-import 'package:peerpal/chat/domain/usecases/get_users_chats.dart';
-import 'package:peerpal/chat/presentation/chat/chat_page/bloc/chat_page_bloc.dart';
+import 'package:peerpal/chat/domain/usecases/get_all_userchats.dart';
+import 'package:peerpal/chat/presentation/chat/chat_page/cubit/chat_page_cubit.dart';
 import 'package:peerpal/chat/presentation/chat/chat_page/view/chat_page_content.dart';
 import 'package:peerpal/discover_feed/data/repository/app_user_repository.dart';
 import 'package:peerpal/discover_setup/pages/discover_communication/domain/get_user_usecase.dart';
@@ -22,16 +20,14 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => true,
-      child: BlocProvider<ChatPageBloc>(
-        create: (context) => ChatPageBloc(
-          getMessagesForChatUseCase:
-              GetChatMessagesUseCase(context.read<ChatRepository>()),
-          appUserRepository: context.read<AppUserRepository>(),
-          authenticationRepository: context.read<AuthenticationRepository>(),
-          getChats: sl<GetUsersChats>(),
-          getAuthenticatedUser: sl<GetAuthenticatedUser>(),
-          chatPartnerId: userId,
-        )..add(LoadChatPageEvent(userChat)),
+      child: BlocProvider<ChatPageCubit>(
+        create: (context) => ChatPageCubit(
+            appUserRepository: sl<AppUserRepository>(),
+            chatRepository: sl<ChatRepository>(),
+            getAuthenticatedUser: sl<GetAuthenticatedUser>(),
+            getAllUserChats: sl<GetAllUserChats>(),
+            chatPartnerId: userId)
+          ..loadChat(),
         child: ChatPageContent(),
       ),
     );
