0d6d91262b92778c6763310e1c92611bbb105847
Daniel Kreiter <daniel.kreiter@gmx.de>
2023-09-05 14:43:52 +0200

    rename chat_page to load_chat_page and chat_page_content.dart to load_chat_content

--- lib/chat/presentation/chat/chat_loading/view/load_chat_page.dart
+++ lib/chat/presentation/chat/chat_loading/view/load_chat_page.dart
@@ -4,7 +4,7 @@ import 'package:peerpal/chat/domain/repository/chat_repository.dart';
 import 'package:peerpal/chat/domain/usecase_response/user_chat.dart';
 import 'package:peerpal/chat/domain/usecases/get_all_userchats.dart';
 import 'package:peerpal/chat/presentation/chat/chat_loading/cubit/chat_page_cubit.dart';
-import 'package:peerpal/chat/presentation/chat/chat_loading/view/chat_page_content.dart';
+import 'package:peerpal/chat/presentation/chat/chat_loading/view/load_chat_content.dart';
 import 'package:peerpal/discover_feed/data/repository/app_user_repository.dart';
 import 'package:peerpal/discover_setup/pages/discover_communication/domain/get_user_usecase.dart';
 import 'package:peerpal/setup.dart';
