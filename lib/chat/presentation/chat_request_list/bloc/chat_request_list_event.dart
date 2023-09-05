6529b358494a0a5cdfeef2e3d8c13e00d6acb388
Daniel Kreiter <daniel.kreiter@gmx.de>
2023-09-05 13:33:43 +0200

    Remove chat_request_list_bloc.dart, chat_request_list_event.dart and chat_request_list_state.dart; Replaced by cubit

--- lib/chat/presentation/chat_request_list/bloc/chat_request_list_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'chat_request_list_bloc.dart';
-
-@immutable
-abstract class ChatRequestListEvent extends Equatable {
-  @override
-  List<Object> get props => [];
-}
-
-class ChatRequestListLoaded extends ChatRequestListEvent {}
