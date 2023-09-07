81639cfd120eaaafc386aafd543160ef580a1016
Daniel Kreiter <daniel.kreiter@gmx.de>
2023-09-07 14:43:27 +0200

    Replace dynamic typing with static typing

--- lib/chat/domain/usecases/get_all_userchats.dart
+++ lib/chat/domain/usecases/get_all_userchats.dart
@@ -1,7 +1,9 @@
 import 'package:peerpal/authentication/persistence/authentication_repository.dart';
+import 'package:peerpal/chat/domain/models/chat.dart';
 import 'package:peerpal/chat/domain/repository/chat_repository.dart';
 import 'package:peerpal/chat/domain/usecase_response/user_chat.dart';
 import 'package:peerpal/discover_feed/data/repository/app_user_repository.dart';
+import 'package:peerpal/discover_feed/domain/peerpal_user.dart';
 
 class GetAllUserChats {
   final ChatRepository chatRepository;
@@ -17,9 +19,10 @@ class GetAllUserChats {
     String currentUserId = authenticationRepository.currentUser.id;
     return chatRepository.getChats().asyncMap((chatList) async {
       List<UserChat> userChats = [];
-      for (var chat in chatList) {
-        final partnerId = chat.uids.firstWhere((id) => id != currentUserId);
-        final user = await userRepository.getUser(partnerId);
+      for (Chat chat in chatList) {
+        final String partnerId =
+            chat.uids.firstWhere((id) => id != currentUserId);
+        final PeerPALUser user = await userRepository.getUser(partnerId);
         userChats.add(UserChat(chat: chat, user: user));
       }
       return userChats;
