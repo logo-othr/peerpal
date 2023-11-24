929492ff2ee98653e3d3b28e7533c5560fc14db9
Daniel Kreiter <daniel.kreiter@gmx.de>
2023-11-24 13:48:14 +0100

    Add comments for clarification

--- lib/account_setup/domain/start_rememberme_notifications.dart
+++ lib/account_setup/domain/start_rememberme_notifications.dart
@@ -1,6 +1,7 @@
 import 'package:peerpal/account_setup/data/rememberme_notification_repository.dart';
 import 'package:peerpal/app_logger.dart';
 
+/// Starts a recurring notification that reminds the user to use the app.
 class StartWeeklyAppReminderNotifications {
   AppReminderNotificationRepository repository;
 
@@ -9,7 +10,7 @@ class StartWeeklyAppReminderNotifications {
   }) : this.repository = repository;
 
   Future<void> call() async {
-    logger.i("Activate weekly reminders");
+    logger.i("Schedule weekly reminders...");
     repository.scheduleWeeklyReminders('Wöchentliche Erinnerung - PeerPAL',
         'Hi, wir würden uns freuen, wenn du PeerPAL diese Woche nutzt!');
   }
