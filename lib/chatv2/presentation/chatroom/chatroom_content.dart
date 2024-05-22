7de3cda84b602abf5f82e4986b4b69ddef1a742a
Daniel Kreiter <daniel.kreiter@gmx.de>
2024-05-22 11:28:14 +0200

    add chatroon_content.dart (chat rewrite)

---
+++ lib/chatv2/presentation/chatroom/chatroom_content.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peerpal/chat/presentation/user_detail_page/user_detail_page.dart';
+import 'package:peerpal/chatv2/presentation/chatroom/chatroom_cubit.dart';
+import 'package:peerpal/chatv2/presentation/widgets/chat_header.dart';
+
+class ChatroomContent extends StatelessWidget {
+  ChatroomContent({
+    Key? key,
+  }) : super(key: key);
+  final TextEditingController _textEditingController = TextEditingController();
+  final FocusNode _focus = FocusNode();
+
+  @override
+  Widget build(BuildContext chatroomContext) {
+    return Scaffold(
+      body: SafeArea(
+        child: BlocBuilder<ChatroomCubit, ChatroomState>(
+            builder: (context, chatroomState) {
+              if (chatroomState is ChatroomInitial) {
+                return _chatroomInitial();
+              } else if (chatroomState is ChatroomLoading) {
+                return _chatroomLoading(chatroomState, chatroomContext);
+              } else if (chatroomState is ChatroomUninitialized) {
+                return _chatroomUninitialized;
+              } else if chatroomState is ChatroomLoaded) {
+                return _chatroomLoaded();
+              }
+            }),
+      ),
+    );
+  }
+
+  Widget _chatroomInitial() {
+    return Center(child: CircularProgressIndicator());
+  }
+
+
+  Widget _chatroomLoading(ChatroomLoading state, BuildContext chatroomContext) {
+    return Column(
+      children: [
+        ChatHeader(chatPartner: state.chatPartner,
+          onBackButtonPressed
+              : () => Navigator.of(chatroomContext).pop(),
+          onBarPressed: () =>
+              Navigator.push(
+                chatroomContext,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      UserInformationPage(
+                        state.chatPartner.id!,
+                        hasMessageButton: false,
+                      ),
+                ),
+              ),)
+      ],
+    );
+  }
+
+  Widget _chatroomUninitialized() {
+    return Container(child: Text("ChatroomUninitialized"));
+  }
+
+  Widget _chatroomLoaded() {
+    return Container(child: Text("ChatroomLoaded"));
+  }
+
+
+}
