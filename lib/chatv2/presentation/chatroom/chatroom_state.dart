4d97b5da8d7cd184b42fdc98b1700f53b362c325
Daniel Kreiter <daniel.kreiter@gmx.de>
2024-05-22 11:28:43 +0200

    add chatroom_state.dart (chat rewrite)

---
+++ lib/chatv2/presentation/chatroom/chatroom_state.dart
@@ -0,0 +1,49 @@
+part of 'chatroom_cubit.dart';
+
+abstract class ChatroomState extends Equatable {
+  final PeerPALUser chatPartner;
+  final PeerPALUser appUser;
+
+  const ChatroomState({required this.chatPartner, required this.appUser});
+
+  List<Object> get props => [chatPartner, appUser];
+}
+
+class ChatroomInitial extends ChatroomState {
+  ChatroomInitial({required chatPartner, required appUser})
+      : super(chatPartner: chatPartner, appUser: appUser);
+
+  @override
+  List<Object> get props => [chatPartner, appUser];
+}
+
+class ChatroomLoading extends ChatroomState {
+  ChatroomLoading({required chatPartner, required appUser})
+      : super(chatPartner: chatPartner, appUser: appUser);
+
+  @override
+  List<Object> get props => [chatPartner, appUser];
+}
+
+class ChatUninitialized extends ChatroomState {
+  ChatUninitialized({required chatPartner, required appUser})
+      : super(chatPartner: chatPartner, appUser: appUser);
+
+  @override
+  List<Object> get props => [chatPartner, appUser];
+}
+
+class ChatLoaded extends ChatroomState {
+  final Chat chat;
+  final List<ChatMessage> messages;
+
+  ChatLoaded(
+      {required this.chat,
+      required this.messages,
+      required chatPartner,
+      required appUser})
+      : super(chatPartner: chatPartner, appUser: appUser);
+
+  @override
+  List<Object> get props => [chat, messages, chatPartner, appUser];
+}
