0308720762f317aaaaa4be2d4c8b46be85bff66f
Daniel Kreiter <daniel.kreiter@gmx.de>
2024-05-24 12:08:12 +0200

    Refactor: Move the SendFriendRequestButton class to a separate file.

---
+++ lib/chatv2/presentation/widgets/friend_request_button/send_friend_request_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:peerpal/chatv2/presentation/widgets/friend_request_button/friend_request_button_layout.dart';
+
+class SendFriendRequestButton extends StatelessWidget {
+  final String buttonText;
+  final VoidCallback onPressed;
+
+  const SendFriendRequestButton({
+    required this.buttonText,
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: FriendRequestButtonLayout(
+        buttonText: buttonText,
+        actionWidget: SizedBox(), // No additional action needed for send button
+      ),
+    );
+  }
+}
