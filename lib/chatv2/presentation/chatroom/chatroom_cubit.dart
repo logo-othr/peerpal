43c4b8d54bda707c880dd9a174eb3452ea5eafd0
Daniel Kreiter <daniel.kreiter@gmx.de>
2024-05-31 11:10:57 +0200

    Add isChatRequest and sendMessage

--- lib/chatv2/presentation/chatroom/chatroom_cubit.dart
+++ lib/chatv2/presentation/chatroom/chatroom_cubit.dart
@@ -1,64 +1,91 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:collection/collection.dart';
 import 'package:equatable/equatable.dart';
-import 'package:peerpal/chat/presentation/chat/chat_loading/cubit/chat_page_cubit.dart';
+import 'package:peerpal/chatv2/domain/core-usecases/send_chat_request_response.dart';
+import 'package:peerpal/chatv2/domain/enums/message_type.dart';
 import 'package:peerpal/chatv2/domain/models/chat.dart';
 import 'package:peerpal/chatv2/domain/models/chat_message.dart';
 import 'package:peerpal/chatv2/domain/usecases/get_chats.dart';
 import 'package:peerpal/chatv2/domain/usecases/get_messages.dart';
 import 'package:peerpal/chatv2/domain/usecases/get_user.dart';
+import 'package:peerpal/chatv2/domain/usecases/send_message.dart';
 import 'package:peerpal/discover_feed/domain/peerpal_user.dart';
 import 'package:peerpal/discover_setup/pages/discover_communication/domain/get_user_usecase.dart';
 
 part 'chatroom_state.dart';
 
 class ChatroomCubit extends Cubit<ChatroomState> {
-  final GetChats getChats;
-  final GetAuthenticatedUser getAppUser;
-  final GetUser getUser;
-  final GetMessages getMessages;
+  final GetChats _getChats;
+  final GetAuthenticatedUser _getAppUser;
+  final GetUser _getUser;
+  final GetMessages _getMessages;
+  final SendMessage _sendMessage;
+  final SendChatRequestResponse _sendChatRequestResponse;
 
   late final StreamSubscription? _messageStream;
   late final StreamSubscription? _chatsStream;
 
-  ChatroomCubit(
-      {required this.getChats,
-      required this.getAppUser,
-      required this.getUser,
-      required this.getMessages})
-      : super(ChatroomInitial(
+  ChatroomCubit({required GetChats getChats,
+    required GetAuthenticatedUser getAppUser,
+    required GetUser getUser,
+    required GetMessages getMessages,
+    required SendMessage sendMessage,
+    required SendChatRequestResponse sendChatRequestResponse})
+      : this._getChats = getChats,
+        this._getAppUser = getAppUser,
+        this._getUser = getUser,
+        this._getMessages = getMessages,
+        this._sendMessage = sendMessage,
+        this._sendChatRequestResponse = sendChatRequestResponse,
+        super(ChatroomInitial(
             chatPartner: PeerPALUser.empty, appUser: PeerPALUser.empty));
 
   Future<void> loadChatroom(String? chatPartnerId) async {
     // Return if the page is already initialized
-    if (!(state is ChatPageInitial)) return;
+    if (!(state is ChatroomInitial)) return;
 
-    var appUser = (await getAppUser());
-    var chatPartner = (await getUser(
+    PeerPALUser appUser = (await _getAppUser());
+    PeerPALUser chatPartner = (await _getUser(
         chatPartnerId!)); // TODO: Throw exception if no user is found
     emit(ChatroomLoading(chatPartner: chatPartner, appUser: appUser));
 
     // Listen to all the user's chats.
     // In this way, we can react to changes such as
     // if the chat no longer exists
-    _chatsStream = getChats(state.appUser.id!).listen(_onChatsUpdate);
+    _chatsStream = _getChats(state.appUser.id!).listen(_onChatsUpdate);
   }
 
-  _onChatsUpdate(chats) async {
+  _onChatsUpdate(List<Chat> chats) async {
     Chat? chat = chats.firstWhereOrNull((Chat chat) => _chatExists(chat));
 
     if (chat == null) {
       _setChatUninitializedState();
     } else {
-      emit(ChatLoaded(
+      emit(ChatroomLoaded(
         messages: [],
         chatPartner: state.chatPartner,
         chat: chat,
         appUser: state.appUser,
       ));
-      _messageStream = getMessages(chat.chatId).listen(_onMessagesUpdate);
+      _messageStream = _getMessages(chat.chatId).listen(_onMessagesUpdate);
     }
+  }
+
+  bool isChatRequest() {
+    ChatroomLoaded loadedState = state as ChatroomLoaded;
+    Chat chat = loadedState.chat;
+    bool chatNotStartedByUser = chat.startedBy != state.appUser.id;
+    bool chatNotAccepted = !chat.chatRequestAccepted;
+    return (chatNotStartedByUser && chatNotAccepted);
+  }
+
+  Future<void> sendChatRequestResponse(bool response) async {
+    ChatroomLoaded loadedState = state as ChatroomLoaded;
+    Chat chat = loadedState.chat;
+    String chatPartnerId = loadedState.chatPartner.id!;
+    await _sendChatRequestResponse(chatPartnerId, chat.chatId, response);
   }
 
   bool _chatExists(Chat chat) {
@@ -72,13 +99,22 @@ class ChatroomCubit extends Cubit<ChatroomState> {
   }
 
   _onMessagesUpdate(List<ChatMessage> messages) {
-    ChatLoaded loadedState = state as ChatLoaded;
-    emit(ChatLoaded(
+    ChatroomLoaded loadedState = state as ChatroomLoaded;
+    emit(ChatroomLoaded(
       messages: messages,
       chatPartner: loadedState.chatPartner,
       chat: loadedState.chat,
       appUser: loadedState.appUser,
     ));
+  }
+
+  Future<void> sendMessage(String text, MessageType messageType) async {
+    if (state is ChatroomUninitialized) {
+      await _sendMessage(state.appUser.id!, null, text, MessageType.text);
+    } else if (state is ChatroomLoaded) {
+      await _sendMessage(state.appUser.id!,
+          (state as ChatroomLoaded).chat.chatId, text, MessageType.text);
+    }
   }
 
   @override
