65abc3ec4a85da0ed98f7ff2b2fd9cc8b55b1c8e
Daniel Kreiter <daniel.kreiter@gmx.de>
2024-05-31 11:11:47 +0200

    Replace dependencies with cubit which is already added in setup.dart

--- lib/chatv2/presentation/chatroom/chatroom_page.dart
+++ lib/chatv2/presentation/chatroom/chatroom_page.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:peerpal/chatv2/domain/usecases/get_chats.dart';
-import 'package:peerpal/chatv2/domain/usecases/get_messages.dart';
-import 'package:peerpal/chatv2/domain/usecases/get_user.dart';
 import 'package:peerpal/chatv2/presentation/chatroom/chatroom_content.dart';
 import 'package:peerpal/chatv2/presentation/chatroom/chatroom_cubit.dart';
-import 'package:peerpal/discover_setup/pages/discover_communication/domain/get_user_usecase.dart';
+import 'package:peerpal/setup.dart';
 
 class ChatroomPage extends StatelessWidget {
-  final String? chatId;
+  final String? chatPartnerId;
 
-  const ChatroomPage({required this.chatId, Key? key}) : super(key: key);
+  const ChatroomPage({required this.chatPartnerId, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => true,
       child: BlocProvider<ChatroomCubit>(
-        create: (context) => ChatroomCubit(
-          getChats: context.read<GetChats>(),
-          getAppUser: context.read<GetAuthenticatedUser>(),
-          getUser: context.read<GetUser>(),
-          getMessages: context.read<GetMessages>(),
-        )..loadChatroom(chatId),
+        create: (context) => sl<ChatroomCubit>()..loadChatroom(chatPartnerId),
         child: ChatroomContent(),
       ),
     );
